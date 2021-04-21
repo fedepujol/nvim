6d17ccd5731fc5b9c7512fca99c9e1d8228844fa
fedepujol <fpujol@protonmail.com>
2021-04-21 17:15:13 -0300

    Change location of colors. Add basic colorscheme

---
+++ lua/theme/colorscheme.lua
@@ -0,0 +1,8 @@
+-- Basic Colorschemes
+vim.api.nvim_exec([[
+    hi Normal guibg=#1E1E1E
+    hi CursorLineNr guifg=#FFFFFF guibg=#262626
+    hi CursorLine guibg=#262626
+    hi LineNr guifg=#B1B1B1 guibg=#1E1E1E
+    hi LspDiagnosticDefaultHint guifg=#42A5F5'
+]], false)
