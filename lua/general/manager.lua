2da39d7c2478d43d53df34d89d53ecd62ac2aef6
fedepujol <fpujol@protonmail.com>
2021-04-22 18:47:17 -0300

    Add Telescope. Re-order and comment each pluggin purpose

--- lua/general/manager.lua
+++ lua/general/manager.lua
@@ -1,15 +1,39 @@
--- Setting Paq
+-- Settings Paq
 
+-- Paq Init
 vim.cmd 'packadd paq-nvim'
 local paq = require 'paq-nvim'.paq
 paq {'savq/paq-nvim', opt = true}
 
+-- Lsp
 paq {'neovim/nvim-lspconfig'}
 paq {'glepnir/lspsaga.nvim'}
+paq {'kabouzeid/nvim-lspinstall'}
+
+-- Icons
 paq {'kyazdani42/nvim-web-devicons'}
-paq {'romgrk/barbar.nvim'}
+
+-- Buffer Bar
+paq {'romgrk/barbar.nvim'} -- Find a replace
+
+-- Hex/RGB/Color Handler
 paq {'norcalli/nvim-colorizer.lua'}
+
+-- Completion
 paq {'hrsh7th/nvim-compe'}
 paq {'hrsh7th/vim-vsnip'}
+
+-- Colors
 paq {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+
+-- StatusLine
 paq {'glepnir/galaxyline.nvim', branch = 'main'}
+
+-- Finder
+-- View Telescope
+paq {'nvim-lua/plenary.nvim'}
+paq {'nvim-lua/popup.nvim'}
+paq {'nvim-telescope/telescope.nvim'}
+
+-- Code Versioning
+-- View GitSings/GitGutter
