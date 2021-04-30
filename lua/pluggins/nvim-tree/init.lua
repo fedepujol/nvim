4eac6cef2bba8e96ecb639858859c5316d366fe9
fedepujol <federicodpujol@gmail.com>
2021-04-30 08:49:54 -0300

    Customize nvim-tree

--- lua/pluggins/nvim-tree/init.lua
+++ lua/pluggins/nvim-tree/init.lua
@@ -1,4 +1,6 @@
 -- Nvim Tree Settings
+local specialFiles = {'README.md', 'Makefile', 'MAKEFILE'}
+
 vim.g.nvim_tree_side = 'left'
 vim.g.nvim_tree_width = 40
 vim.g.nvim_tree_auto_open = 1
@@ -7,5 +9,33 @@ vim.g.nvim_tree_disable_netrw = 1
 vim.g.nvim_tree_hide_dotfiles = 1
 vim.g.nvim_tree_add_trailing = 1
 vim.g.nvim_tree_group_empty = 1
---vim.g.nvim_tree_special_files = ['README.md', 'Makefile']
-
+vim.g.nvim_tree_lsp_diagnostics = 0
+vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_special_files = specialFiles
+vim.g.nvim_tree_icons = {
+	default = '',
+	symlink = '',
+	git = {
+		unstaged = "Us",
+		staged = "S",
+		unmerged = "Um",
+		renamed = "R",
+		untracked = "U",
+		deleted = "D",
+		ignored = "I"
+	},
+	folder = {
+		default = "",
+		open = "",
+		empty = "",
+		empty_open = "",
+		symlink = "",
+		symlink_open = "",
+	},
+	lsp = {
+		hint = "",
+		info = "",
+		warning = "",
+		error = "",
+	}
+}
