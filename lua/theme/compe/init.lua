60c51b0c92433ac4298f1a6613c003f8ba306602
fedepujol <fpujol@protonmail.com>
2021-04-30 18:23:15 -0300

    Update Pmenu colorscheme to be in a gray scale

--- lua/theme/compe/init.lua
+++ lua/theme/compe/init.lua
@@ -1,4 +1,7 @@
 -- Compe Colorscheme
 vim.api.nvim_exec([[
-    hi Pmenu guibg=#191919
+    hi Pmenu       guibg=#262626
+    hi PmenuSel    guibg=#2F2F2F
+    hi PmenuSbar   guibg=#808080
+    hi PmenuThumb  guibg=#6C6C6C
 ]], false)
