6b24d206d71639280aff76a00709dd72941de468
fedepujol <fpujol@protonmail.com>
2021-04-30 18:24:02 -0300

    Removed keybinding of oldfiles and added one for help_tags

--- lua/pluggins/telescope/init.lua
+++ lua/pluggins/telescope/init.lua
@@ -32,5 +32,5 @@ local opts = { noremap = true }
 vim.api.nvim_set_keymap('n', '<C-f>f', "<CMD>lua require('telescope.builtin').find_files({previewer = false})<CR>", opts)
 vim.api.nvim_set_keymap('n', '<C-f>g', "<CMD>lua require('telescope.builtin').live_grep()<CR>", opts)
 vim.api.nvim_set_keymap('n', '<C-f>b', "<CMD>lua require('telescope.builtin').file_browser()<CR>", opts)
-vim.api.nvim_set_keymap('n', '<C-f>p', "<CMD>lua require('telescope.builtin').oldfiles()<CR>", opts)
+vim.api.nvim_set_keymap('n', '<C-f>h', "<CMD>lua require('telescope.builtin').help_tags()<CR>", opts)
 vim.api.nvim_set_keymap('n', '<C-k><C-s>', "<CMD>lua require('telescope.builtin').keymaps()<CR>", opts)
