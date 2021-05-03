1af9575e90261a71125139c4a6405669885c2d81
fedepujol <fpujol@protonmail.com>
2021-05-03 19:24:09 -0300

    Change folder icons

--- lua/pluggins/nvim-tree/init.lua
+++ lua/pluggins/nvim-tree/init.lua
@@ -1,17 +1,18 @@
 -- Nvim Tree Settings
 local specialFiles = {'README.md', 'Makefile', 'MAKEFILE'}
 
-vim.g.nvim_tree_side = 'left'
-vim.g.nvim_tree_width = 30
+vim.g.nvim_tree_add_trailing = 1
 vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_disable_netrw = 1
+vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_group_empty = 1
 vim.g.nvim_tree_hide_dotfiles = 1
 vim.g.nvim_tree_indent_markers = 1
-vim.g.nvim_tree_add_trailing = 1
-vim.g.nvim_tree_group_empty = 1
 vim.g.nvim_tree_lsp_diagnostics = 0
-vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_side = 'left'
 vim.g.nvim_tree_special_files = specialFiles
+vim.g.nvim_tree_width = 30
+
 vim.g.nvim_tree_icons = {
 	default = '',
 	symlink = '',
