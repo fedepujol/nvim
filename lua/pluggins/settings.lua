fe43d926b56e354a3018f7543de8988cc30217f5
fedepujol <fpujol@protonmail.com>
2021-05-03 19:25:51 -0300

    Add missing info on options

--- lua/pluggins/settings.lua
+++ lua/pluggins/settings.lua
@@ -2,7 +2,7 @@
 local o = vim.o
 
 -- Global Options
-o.termguicolors = true                  -- Set for colorizer. Enables 24-Bit Colors on TUI.
-o.completeopt = "menuone,noselect"      -- Set for nvim-compe. Comma-separated options for Insert mode.
-o.showmode = false
-o.laststatus = 2
+o.termguicolors = true 					-- Set for colorizer. Enables 24-Bit Colors on TUI.
+o.completeopt = "menuone,noselect" 		-- Set for nvim-compe. Comma-separated options for Insert mode.
+o.showmode = false 						-- In Insert/Replace/Visual put a message on the last line.
+o.laststatus = 2 						-- Influence if the last window should have a statusline. 2 -> always.
