8881a72cda5a9087567831fd74e739e26f015f66
fedepujol <fpujol@protonmail.com>
2021-05-03 19:22:49 -0300

    Small changes. Added line-swaps and tab lines in visual mode

--- lua/general/mappings.lua
+++ lua/general/mappings.lua
@@ -1,9 +1,6 @@
 -- General Mappings
 local opts = { noremap = true }
 
-vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', opts)
-vim.g.mapleader = ' '
-
 -- Use ALT + hjkl to resize windows
 vim.api.nvim_set_keymap('n', '<M-j>', ':resize -2<CR>', opts)
 vim.api.nvim_set_keymap('n', '<M-k>', ':resize +2<CR>', opts)
@@ -19,8 +16,8 @@ vim.api.nvim_set_keymap('n', '<C-q>', ':q<CR>', opts)
 vim.api.nvim_set_keymap('i', 'jk', '<ESC><CR>', opts)
 
 -- Better Tabbing
-vim.api.nvim_set_keymap('v', '<', '<gv', opts)
-vim.api.nvim_set_keymap('v', '>', '>gv', opts)
+vim.api.nvim_set_keymap('v', '<S-TAB>', '<gv', opts)
+vim.api.nvim_set_keymap('v', '<TAB>', '>gv', opts)
 
 -- Window Nav
 vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', opts)
@@ -28,5 +25,9 @@ vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', opts)
 vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', opts)
 vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', opts)
 
--- I Hate Help Split Below
+-- Split Help vertical
 vim.api.nvim_command("ca h vert h")
+
+-- Move cursor line up or down
+vim.api.nvim_set_keymap('n', '<C-M-k>', ':normal! dd2kp<CR>', opts)
+vim.api.nvim_set_keymap('n', '<C-M-j>', ':normal! ddp<CR>', opts)
