24733dae00dd896123f098fccbedc27e39b89582
fedepujol <fpujol@protonmail.com>
2021-05-07 19:10:15 -0300

    Default keymaps to use snippets

--- lua/pluggins/compe/init.lua
+++ lua/pluggins/compe/init.lua
@@ -20,6 +20,7 @@ require'compe'.setup {
       nvim_lsp = true;
       nvim_lua = true;
       vsnip = true;
+	  tags = true;
     };
   }
 
@@ -60,6 +61,7 @@ require'compe'.setup {
     end
   end
 
+  vim.cmd("inoremap <silent><expr> <CR> compe#confirm('<CR>')")
   vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
   vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
   vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
