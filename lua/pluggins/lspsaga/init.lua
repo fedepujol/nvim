e0e5ae234485e22496f1bcb394a1c21195b275cf
fedepujol <federicodpujol@gmail.com>
2021-05-10 00:36:13 -0300

    Change icons to match the galaxyline

--- lua/pluggins/lspsaga/init.lua
+++ lua/pluggins/lspsaga/init.lua
@@ -1,9 +1,9 @@
 local saga = require 'lspsaga'
 
 saga.init_lsp_saga{
-    error_sign = '',
-    warn_sign = '',
-    hint_sign = '',
+    error_sign = '',
+    warn_sign = '',
+    hint_sign = 'W',
     infor_sign = ''
 }
 
