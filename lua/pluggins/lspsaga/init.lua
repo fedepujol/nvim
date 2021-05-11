1b05585784829211eab9dede75c722c194da2796
fedepujol <fpujol@protonmail.com>
2021-05-10 21:33:47 -0300

    Changes in keybindings

--- lua/pluggins/lspsaga/init.lua
+++ lua/pluggins/lspsaga/init.lua
@@ -10,19 +10,21 @@ saga.init_lsp_saga{
 local opts = {noremap = true, silent = true}
 
 -- Lsp Finder (find cursor word definition and reference)
-vim.api.nvim_set_keymap('n', 'gh', ':Lspsaga lsp_finder<CR>', opts)
+vim.api.nvim_set_keymap('n', 'lf', ':Lspsaga lsp_finder<CR>', opts)
 
 -- Code Actions (Organize imports, import missing libraries, etc)
 vim.api.nvim_set_keymap('n', '<leader>ca', ':Lspsaga code_action<CR>', opts)
+vim.api.nvim_set_keymap('v', '<leader>ra', ':<C-U>Lspsaga range_code_action<CR>', opts)
 
 -- Hover (show documentation and scroll)
-vim.api.nvim_set_keymap('n', 'K', ':Lspsaga hover_doc<CR>', opts)
-vim.api.nvim_set_keymap('n', '<C-f>', "<Cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>", opts)
+vim.api.nvim_set_keymap('n', 'H', ':Lspsaga hover_doc<CR>', opts)
+vim.api.nvim_set_keymap('n', '<M-f>', "<Cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>", opts)
+vim.api.nvim_set_keymap('n', '<M-b>', "<Cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>", opts)
 
 -- Signature (Show Signature Help)
 vim.api.nvim_set_keymap('n', 'gs', ':Lspsaga signature_help<CR>', opts)
 
 -- Diagnostics
 vim.api.nvim_set_keymap('n', '<leader>cd', ':Lspsaga show_line_diagnostics<CR>', opts)
-vim.api.nvim_set_keymap('n', '<C-n>', ':Lspsaga diagnostic_jump_next<CR>', opts)
-vim.api.nvim_set_keymap('n', '<C-m>', ':Lspsaga diagnostic_jump_prev<CR>', opts)
+vim.api.nvim_set_keymap('n', '<C-n>', ':', opts)
+vim.api.nvim_set_keymap('n', '<C-m>', ':', opts)
