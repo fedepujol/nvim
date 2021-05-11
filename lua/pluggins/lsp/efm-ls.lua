f03a948a22526f8981ebb41b255c82cf750b25d4
fedepujol <fpujol@protonmail.com>
2021-05-10 21:33:25 -0300

    Update EFM to work with HTML, CSS, JSON, JS and LUA

--- lua/pluggins/lsp/efm-ls.lua
+++ lua/pluggins/lsp/efm-ls.lua
@@ -1,14 +1,41 @@
 -- EFM LSP
 -- go get github.com/mattn/efm-language
+local markdownPandoc = {
+	formatCommand = 'pandoc -f markdown -t gfm -sp --tab-stop=4'
+}
+
+local html = {
+	formatCommand = './node_modules/.bin/prettier ${--tab-width:tabWidth} ${--single-quote:singleQuote} --parser html'
+}
+
+local css = {
+	formatCommand = './node_modules/.bin/prettier ${--tab-width:tabWidth} ${--single-quote:singleQuote} --parser css'
+}
+
+local json = {
+	formatCommand = './node_modules/.bin/prettier ${--tab-width:tabWidth} --parser json'
+}
+
+local lua = {
+	formatCommand = 'lua-format -i',
+	formatStdin = true,
+}
 
 require('lspconfig').efm.setup{
-	init_options = { documentFormatting = true },
+	init_options = {
+		documentFormatting = true,
+		hover = true,
+		documentSymbol = true,
+		codeAction = true,
+	},
 	settings = {
 		rootMarkers = {".git/"},
 		languages = {
-			lua = {
-				{formatCommand = "lua-format -i", formatStdin = true},
-			}
+			css = css,
+			html = html,
+			json = json,
+			lua = lua,
+			markdown = markdownPandoc,
 		}
 	}
 }
