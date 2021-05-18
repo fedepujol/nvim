6a2565d75c22bf2fc674d91d881c26ab7323201a
fedepujol <federicodpujol@gmail.com>
2021-05-17 23:36:00 -0300

    Fix function

--- lua/pluggins/lsp/efm-ls.lua
+++ lua/pluggins/lsp/efm-ls.lua
@@ -2,7 +2,7 @@
 -- go get github.com/mattn/efm-language
 local lsp = require('pluggins.lsp')
 require('lspconfig').efm.setup{
-	cmd = { lsp.data_dir.."/lspinstall/efm/efm-langserver.exe", "-c="..lsp.config_dir.."efm-langserver/config.yml" },
+	cmd = { lsp.data_dir.."/lspinstall/efm/efm-langserver", "-c="..lsp.config_dir().."efm-langserver/config.yml" },
 	init_options = { documentFormatting = true },
 	filetypes = {'css', 'html', 'json', 'lua', 'markdown', 'yaml'},
 }
