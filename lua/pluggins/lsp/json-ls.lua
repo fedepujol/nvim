b49d08d7925639d7e149cec1d580a44b1bfd6ed4
fedepujol <fpujol@protonmail.com>
2021-05-26 23:57:50 -0300

    Update JSON LSP

--- lua/pluggins/lsp/json-ls.lua
+++ lua/pluggins/lsp/json-ls.lua
@@ -1,6 +1,10 @@
 -- JSON LSP
 -- npm i -g vscode-json-languageserver
+local lsp = require('pluggins.lsp')
+
 require 'lspconfig'.jsonls.setup {
+	cmd = {'node',  lsp.data_dir..'/json/vscode-json/json-language-features/server/dist/node/jsonServerMain.js', '--stdio'},
+	root_dir = require('lspconfig/util').root_pattern{'.git', vim.fn.getcwd()},
     commands = {
       Format = {
         function()
