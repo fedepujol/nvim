b691e7028001b0f034d0ca356337e7c7d248406f
fedepujol <fpujol@protonmail.com>
2021-06-14 18:08:18 -0300

    Change right side of status line. Add some colors and change how to display line, column and perecentage

--- lua/pluggins/galaxyline/init.lua
+++ lua/pluggins/galaxyline/init.lua
@@ -16,101 +16,101 @@ local distro = 'arch'
 ------------------------ Left Section  ------------------------
 ---------------------------------------------------------------
 gLineSection.left[0] = {
-    CustomIcon = {
-        provider = function()
-            return '  '..icon.getIcon(distro) ..' '
-        end,
-        highlight = icon.getColor(distro),
-    }
+	CustomIcon = {
+		provider = function()
+			return '  '..icon.getIcon(distro) ..' '
+		end,
+		highlight = icon.getColor(distro),
+	}
 }
 
 gLineSection.left[1] = {
-    ViMode = {
-        provider = function ()
-            vim.api.nvim_command('hi GalaxyViMode guifg='..vimMode.getModeColor().fg..' guibg='..vimMode.getModeColor().bg..' gui=bold')
-            return '  '..vimMode.getModeAlias()..' '
-        end,
-        separator = ' ',
-        highlight = { colors.white, colors.black3 },
-        separator_highlight = { colors.white, colors.black3 },
-    }
+	ViMode = {
+		provider = function ()
+			vim.api.nvim_command('hi GalaxyViMode guifg='..vimMode.getModeColor().fg..' guibg='..vimMode.getModeColor().bg..' gui=bold')
+			return '  '..vimMode.getModeAlias()..' '
+		end,
+		separator = ' ',
+		highlight = { colors.white, colors.black3 },
+		separator_highlight = {colors.white, colors.black3},
+	}
 }
 
 gLineSection.left[2] = {
-    FileIcon = {
-        provider = 'FileIcon',
-        condition = condition.buffer_not_empty,
-        highlight = { file_info.get_file_icon_color, colors.black3 },
-    }
+	FileIcon = {
+		provider = 'FileIcon',
+		condition = condition.buffer_not_empty,
+		highlight = { file_info.get_file_icon_color, colors.black3 },
+	}
 }
 
 gLineSection.left[3] = {
-    FileName = {
-        provider = 'FileName',
-        condition = condition.buffer_not_empty,
-        highlight = { file_info.get_file_icon_color, colors.black3 },
-    }
+	FileName = {
+		provider = 'FileName',
+		condition = condition.buffer_not_empty,
+		highlight = { file_info.get_file_icon_color, colors.black3 },
+	}
 }
 
 gLineSection.left[4] = {
-    FileSize = {
-        provider = 'FileSize',
-        condition = condition.buffer_not_empty,
-        icon = ' ',
-        highlight = { colors.green5, colors.black3 },
-    }
+	FileSize = {
+		provider = 'FileSize',
+		condition = condition.buffer_not_empty,
+		icon = ' ',
+		highlight = { colors.green5, colors.black3 },
+	}
 }
 
 gLineSection.left[5] = {
-    DiagnosticInfo  = {
-        provider = function ()
+	DiagnosticInfo  = {
+		provider = function ()
 			local wc = vim.lsp.diagnostic.get_count(0, [[Warning]])
 			local ec = vim.lsp.diagnostic.get_count(0, [[Error]])
 			if wc + ec == 0 then
-	            return ''
+				return ''
 			end
-        end,
-        condition = condition.buffer_not_empty,
-        highlight = { colors.green5, colors.black3 },
-    }
+		end,
+		condition = condition.buffer_not_empty,
+		highlight = { colors.green5, colors.black3 },
+	}
 }
 
 gLineSection.left[6] = {
-    DiagnosticError  = {
-        provider = function()
-            local count = vim.lsp.diagnostic.get_count(0, [[Error]])
+	DiagnosticError  = {
+		provider = function()
+			local count = vim.lsp.diagnostic.get_count(0, [[Error]])
 			if count > 0 then
-            	return ' '..count
+				return ' '..count
 			end
-        end,
+		end,
 		condition = condition.buffer_not_empty,
-        highlight = { colors.red4, colors.black3, 'bold' }
-    }
+		highlight = { colors.red4, colors.black3, 'bold' }
+	}
 }
 
 gLineSection.left[7] = {
-    DiagnosticWarn  = {
-        provider = function ()
-            local count = vim.lsp.diagnostic.get_count(0, [[Warning]])
+	DiagnosticWarn  = {
+		provider = function ()
+			local count = vim.lsp.diagnostic.get_count(0, [[Warning]])
 			if count > 0 then
-	            return '   '..count
+				return '   '..count
 			end
-        end,
-        condition = condition.buffer_not_empty,
-        highlight = { colors.yellow3, colors.black3, 'bold'}
-    }
+		end,
+		condition = condition.buffer_not_empty,
+		highlight = { colors.yellow3, colors.black3, 'bold'}
+	}
 }
 
 ---------------------------------------------------------------
 -----------------------  Mid Section   ------------------------
 ---------------------------------------------------------------
 gLineSection.mid[0] = {
-    Branch = {
-        provider = 'GitBranch',
-        condition = condition.buffer_not_empty,
+	Branch = {
+		provider = 'GitBranch',
+		condition = condition.buffer_not_empty,
 		icon = ' ',
-        highlight = { colors.pink5, colors.black3 }
-    }
+		highlight = { colors.pink5, colors.black3 }
+	}
 }
 
 gLineSection.mid[1] = {
@@ -119,8 +119,8 @@ gLineSection.mid[1] = {
 		condition = condition.buffer_not_empty,
 		icon = '+',
 		separator = ' ',
-        highlight = { colors.green1, colors.black3 },
-        separator_highlight = { colors.white, colors.black3 },
+		highlight = { colors.green1, colors.black3 },
+		separator_highlight = { colors.white, colors.black3 },
 	}
 }
 
@@ -129,7 +129,7 @@ gLineSection.mid[2] = {
 		provider = 'DiffModified',
 		condition = condition.buffer_not_empty,
 		icon = '~',
-        highlight = { colors.orange3, colors.black3 }
+		highlight = { colors.orange3, colors.black3 }
 	}
 }
 
@@ -138,49 +138,99 @@ gLineSection.mid[3] = {
 		provider = 'DiffRemove',
 		condition = condition.buffer_not_empty,
 		icon = '-',
-        highlight = { colors.red5, colors.black3 }
+		highlight = { colors.red5, colors.black3 }
 	}
 }
 ---------------------------------------------------------------
 ----------------------- Right Section  ------------------------
 ---------------------------------------------------------------
 gLineSection.right[0] = {
-    GetLspClient  = {
-        provider = 'GetLspClient',
-        condition = condition.buffer_not_empty,
-        icon = ' ',
-        highlight = { colors.white, colors.black3},
-        separator_highlight = {colors.white, colors.black3}
-    }
+	IconLsp = {
+		provider = function()
+			return ''
+		end,
+		condition = condition.buffer_not_empty,
+		highlight = { colors.black4, colors.black3},
+		separator = ' ',
+		separator_highlight = {colors.black4, colors.black3}
+	}
 }
 
 gLineSection.right[1] = {
-    LinePercent = {
-        provider = 'LinePercent',
+	GetLspClient  = {
+		provider = 'GetLspClient',
 		condition = condition.buffer_not_empty,
-        separator = ' |',
-        highlight = { colors.white, colors.black3},
-        separator_highlight = {colors.white, colors.black3}
-    }
+		icon = ' ',
+		highlight = { colors.gray3, colors.black4},
+	}
 }
 
 gLineSection.right[2] = {
-    LineNumber = {
-        provider = function() return 'L: '..vim.fn.line('.') end,
+	IconLC = {
+		provider = function()
+			return ''
+		end,
 		condition = condition.buffer_not_empty,
-		separator = '| ',
-        highlight = { colors.white, colors.black3},
-        separator_highlight = {colors.white, colors.black3}
-    }
+		highlight = { colors.pink6, colors.black4},
+		separator = ' ',
+		separator_highlight = {colors.gray3, colors.black4}
+	}
 }
 
 gLineSection.right[3] = {
-    ColNumber = {
-        provider = function() return 'C: '..vim.fn.col('.')..' ' end,
+	LineNumber = {
+		provider = function()
+			local l = vim.fn.line('.')
+			local c = vim.fn.col('.')
+			if c < 10 then
+				c = '0'..c
+			end
+			if l < 10 then
+				l = '0'..l
+			end
+			return ' '..l..'.'..c
+		end,
 		condition = condition.buffer_not_empty,
-		separator = ', ',
-        highlight = { colors.white, colors.black3},
-        separator_highlight = {colors.white, colors.black3}
-    }
+		separator = 'Lc ',
+		highlight = { colors.pink6, colors.black4},
+		separator_highlight = {colors.black4, colors.pink6}
+	}
+}
+
+gLineSection.right[4] = {
+	IconLP = {
+		provider = function()
+			return ''
+		end,
+		condition = condition.buffer_not_empty,
+		highlight = { colors.green5, colors.black4},
+		separator = ' ',
+		separator_highlight = {colors.pink6, colors.black4}
+	}
+}
+
+gLineSection.right[5] = {
+	LinePercent = {
+		provider = function()
+			local cL = vim.fn.line('.')
+			local tL = vim.fn.line('$')
+
+			if cL == 1 then
+				return " Top "
+			elseif cL == vim.fn.line('$') then
+				return " Bot "
+			end
+
+			local result, _ = math.modf((cL / tL) * 100)
+			if result < 10 then
+				result = '0'..result
+			end
+			return ' '..result..'% '
+		end,
+		condition = condition.buffer_not_empty,
+		highlight = { colors.green5, colors.black4},
+		separator = ' ',
+		separator_highlight = {colors.black4, colors.green5}
+	}
 }
 
