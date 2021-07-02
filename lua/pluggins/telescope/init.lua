8916795f8d1da6f596fce1a717db17b90ea73ce3
fedepujol <fpujol@protonmail.com>
2021-07-02 19:44:58 -0300

    Update telescope setup (deprecated tags). Add more keybindings to telescope's builtin utilities

--- lua/pluggins/telescope/init.lua
+++ lua/pluggins/telescope/init.lua
@@ -9,12 +9,12 @@ require('telescope').setup{
 			'--column',
 			'--smart-case'
 		},
-		prompt_position = 'top',
 		prompt_prefix = '~ ',
 		selection_caret = '> ',
 		sorting_strategy = 'ascending',
 		layout_strategy = 'horizontal',
-		layout_defaults = {
+		layout_config= {
+			prompt_position = 'top',
 			horizontal = {
 				mirror = false,
 			},
@@ -29,8 +29,18 @@ require('telescope').setup{
 }
 
 local opts = { noremap = true }
+-- File Search
 vim.api.nvim_set_keymap('n', '<C-p>', "<CMD>lua require('telescope.builtin').find_files({previewer = false})<CR>", opts)
+vim.api.nvim_set_keymap('n', '<C-f>b', "<CMD>lua require('telescope.builtin').buffers()<CR>", opts)
+vim.api.nvim_set_keymap('n', '<C-f>o', "<CMD>lua require('telescope.builtin').oldfiles()<CR>", opts)
+-- Word Search
 vim.api.nvim_set_keymap('n', '<C-f>g', "<CMD>lua require('telescope.builtin').live_grep()<CR>", opts)
-vim.api.nvim_set_keymap('n', '<C-f>b', "<CMD>lua require('telescope.builtin').file_browser()<CR>", opts)
 vim.api.nvim_set_keymap('n', '<C-f>h', "<CMD>lua require('telescope.builtin').help_tags()<CR>", opts)
+vim.api.nvim_set_keymap('n', '<C-f>c', "<CMD>lua require('telescope.builtin').grep_string()<CR>", opts)
+-- KeyMaps Search
 vim.api.nvim_set_keymap('n', '<C-k><C-s>', "<CMD>lua require('telescope.builtin').keymaps()<CR>", opts)
+-- Git Search
+vim.api.nvim_set_keymap('n', '<C-g>c', "<CMD>lua require('telescope.builtin').git_bcommits()<CR>", opts)
+vim.api.nvim_set_keymap('n', '<C-g>b', "<CMD>lua require('telescope.builtin').git_branches()<CR>", opts)
+vim.api.nvim_set_keymap('n', '<C-g>s', "<CMD>lua require('telescope.builtin').git_status()<CR>", opts)
+
