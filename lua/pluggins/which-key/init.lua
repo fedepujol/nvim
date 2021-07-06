12cb415f519637de2a1969c1e0de796efce26504
fedepujol <fpujol@protonmail.com>
2021-07-05 22:57:23 -0300

    Map Lsp, General Settings and Git keys

--- lua/pluggins/which-key/init.lua
+++ lua/pluggins/which-key/init.lua
@@ -27,3 +27,36 @@ require('which-key').setup {
 	show_help = true,
 	hidden = {'<silent>', '<cmd>', '<CMD>', '<CR>', 'lua', '^:'},
 }
+
+local wk = require('which-key')
+
+wk.register({
+	['|'] = {
+		name = "Internal Settings",
+		k = {"<cmd>lua require('telescope.builtin').keymaps()<CR>", "Keymaps"},
+	},
+	g = {
+		name = "Git",
+		j = {"<cmd>lua require('gitsigns').next_hunk()<CR>", "Next Hunk"},
+		J = {"<cmd>lua require('gitsigns').prev_hunk()<CR>", "Prev Hunk"},
+		p = {"<cmd>lua require('gitsigns').preview_hunk()<CR>", "Preview Hunk"},
+		b = {"<cmd>lua require('telescope.builtin').git_branches()<CR>", "Checkout Branch"},
+		s = {"<cmd>lua require('telescope.builtin').git_status()<CR>", "Status"},
+		c = {"<cmd>lua require('telescope.builtin').git_bcommits()<CR>", "Branch Commits"}
+	},
+	l = {
+		name = "LSP",
+		i = {":LspInfo<CR>", "Info"},
+		H = {"<cmd>lua vim.lsp.buf.hover()<CR>", "Hover"},
+		d = { '<Cmd>lua vim.lsp.buf.definition()<CR>', "Go To Definition"},
+		I = { '<Cmd>lua vim.lsp.buf.implementation()<CR>', "Go to Implementation"},
+		s = { '<cmd>lua vim.lsp.buf.document_symbol()<CR>', "Document Symbol"},
+		c = { '<cmd>lua vim.lsp.buf.code_action()<CR>', "Show Code Actions"},
+		t = { '<Cmd>lua vim.lsp.buf.type_definition()<CR>', "Go to Type-Definition"},
+		f = { '<Cmd>lua vim.lsp.buf.formatting()<CR>', "Format"},
+		r = { '<cmd>lua vim.lsp.buf.references()<CR>', "Show References"},
+		h = { '<cmd>lua vim.lsp.buf.signature_help()<CR>', "Get Signature Help"},
+		n = { '<cmd>lua vim.lsp.diagnostic.goto_next({popup_opts = {border = "single"}})<CR>', "Next Diagnostic"},
+		p = { '<cmd>lua vim.lsp.diagnostic.goto_prev({popup_opts = {border = "single"}})<CR>', "Prev. Diagnostic"},
+	}
+}, {mode = 'n', prefix = '<leader>', noremap = true, silent = true})
