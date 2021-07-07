1d038e1824445fc8a4f1d616208973bb4e2608ff
fedepujol <fpujol@protonmail.com>
2021-07-07 11:16:54 -0300

    Change defaults to show hidden, not to open by default and width

--- lua/pluggins/nvim-tree/init.lua
+++ lua/pluggins/nvim-tree/init.lua
@@ -4,19 +4,18 @@ local ignored = {'.git', 'node_modules'}
 
 vim.g.nvim_tree_add_trailing = 1 				-- Append a trailing dash to folders
 vim.g.nvim_tree_auto_close = 1 					-- Closes the tree when its the last window
-vim.g.nvim_tree_auto_open = 1 					-- Open NvimTree when typing vim $DIR or vim
+vim.g.nvim_tree_auto_open = 0 					-- Open NvimTree when typing vim $DIR or vim
 vim.g.nvim_tree_follow = 1 						-- Allows the cursor to be updated when entering another buffer
 vim.g.nvim_tree_disable_netrw = 1 				-- Disables netrw
 vim.g.nvim_tree_git_hl = 1 						-- Enable git hightlights
 vim.g.nvim_tree_group_empty = 1 				-- Compact folders that only contain a single folder
-vim.g.nvim_tree_hide_dotfiles = 1 				-- Hide dotfiles
+vim.g.nvim_tree_hide_dotfiles = 0 				-- Hide dotfiles
 vim.g.nvim_tree_ignore = ignored 				-- Tell NvimTree to ingore this
 vim.g.nvim_tree_indent_markers = 1 				-- Show indent marks
 vim.g.nvim_tree_lsp_diagnostics = 0 			-- Dont show LspDiagnostics info in sign column
 vim.g.nvim_tree_side = 'left' 					-- Tree side
 vim.g.nvim_tree_special_files = specialFiles 	-- List of elements that gets highlight with NvimTreeSpecialFile
 vim.g.nvim_tree_width = 60 						-- Default Tree Width
-vim.g.nvim_tree_width_allow_resize = 1 			-- Will resize the tree when opening a file
 
 -- Redefined NvimTree icons for git information
 -- and Folders
