21d78c16049f4a7944c00f307a03c8070d6e92e6
fedepujol <fpujol@protonmail.com>
2021-07-16 23:26:35 -0300

    Remove defaults from config

--- lua/pluggins/lsp/tsserver-ls.lua
+++ lua/pluggins/lsp/tsserver-ls.lua
@@ -4,8 +4,6 @@ local lsp = require('pluggins.lsp')
 
 require('lspconfig').tsserver.setup{
 	cmd = {lsp.lsp_dir..'/tsserver/typescript-language-server.cmd', '--stdio'},
-	filetypes = {'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx'},
-	root_dir = lsp.root_pattern{'package.json', 'tsconfig.json', 'jsconfig.json', '.git'},
 	on_attach = lsp.common_on_attach,
 	handlers = {
 		["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
