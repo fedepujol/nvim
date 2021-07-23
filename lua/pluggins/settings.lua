0c267d6dd1c229d7a47762989b6d6169eb8e5f37
fedepujol <fpujol@protonmail.com>
2021-07-23 10:35:34 -0300

    Remove settings from pluggins to core/settings

--- lua/pluggins/settings.lua
+++
@@ -1,8 +0,0 @@
--- Settings
-local o = vim.o
-
--- Global Options
-o.termguicolors = true 					-- Set for colorizer. Enables 24-Bit Colors on TUI.
-o.completeopt = "menuone,noselect" 		-- Set for nvim-compe. Comma-separated options for Insert mode.
-o.showmode = false 						-- In Insert/Replace/Visual put a message on the last line.
-o.laststatus = 2 						-- Influence if the last window should have a statusline. 2 -> always.
