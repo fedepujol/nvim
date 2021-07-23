33470bfa48647abfb6d173b8a3c44b9c061bcff8
fedepujol <fpujol@protonmail.com>
2021-07-23 10:38:32 -0300

    Add floating options to terminal

--- lua/pluggins/toggleterm/init.lua
+++ lua/pluggins/toggleterm/init.lua
@@ -1,12 +1,22 @@
 -- ToggleTerma Setup
 require('toggleterm').setup{
-	size = 12,
+	size = 15,
 	hide_numbers = true,
 	start_in_insert = false,
-	direction = 'horizontal',
+	direction = 'horizontal', -- vertical | float | window
 	persist_size = true,
 	close_on_exit = true,
-	shell = "bash"
+	shell = "bash",
+	float_opts = {
+		border = "rounded",
+		width = 150,
+		height = 40,
+		winblend = 3,
+		highlights = {
+			border = "Normal",
+			background = "Normal"
+		}
+	}
 }
 
 vim.api.nvim_set_keymap('n', '<leader>t', "<cmd>:ToggleTerm<CR>", {noremap = true, silent = true})
