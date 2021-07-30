7cd25250bcce7f0ebedf93ea281960af97dc0ea1
fedepujol <fpujol@protonmail.com>
2021-07-29 21:17:09 -0300

    Change Haskell root_dir

--- lua/pluggins/lsp/haskell-ls.lua
+++ lua/pluggins/lsp/haskell-ls.lua
@@ -1,4 +1,8 @@
 -- Haskell LSP (hls)
 -- hie and ghcide were merge into hls
 -- To manually install hls visit https://github.com/haskell/haskell-language-server
-require('lspconfig').hls.setup{}
+local lsp = require('pluggins.lsp')
+
+require('lspconfig').hls.setup {
+	root_dir = lsp.root_pattern('.git', '*.hs')
+}
