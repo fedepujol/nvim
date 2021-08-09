322945944147097a7590082e1a113a32c508e086
fedepujol <fpujol@protonmail.com>
2021-08-09 00:27:24 -0300

    Update angular and bash LSP

--- lua/pluggins/lsp/bash-ls.lua
+++ lua/pluggins/lsp/bash-ls.lua
@@ -1,2 +1,10 @@
 -- Bash LSP
-require 'lspconfig'.bashls.setup{}
+local lsp = require('pluggins.lsp')
+
+require('lspconfig').bashls.setup{
+	cmd = {lsp.lsp_dir..'/bash/node_modules/.bin/bash-language-server', 'start'},
+	cmd_env = {
+		GLOB_PATTERN = "*@(.sh|.inc|.bash|.zsh|.command)"
+	},
+	filetypes = {'sh', 'zsh'}
+}
