b4c010642195c2e336079f31eb749e976ddc9181
fedepujol <fpujol@protonmail.com>
2021-08-27 18:21:54 -0300

    [WIP] Create NPM table with commands to pass to Menu

--- lua/pluggins/nui-nvim/npm.lua
+++ lua/pluggins/nui-nvim/npm.lua
@@ -1,6 +1,16 @@
 local Menu = require('nui.menu')
 local Input = require('nui.input')
 
+local Npm = {
+	start = { id = 1, cmd = 'npm start', menu = 'Start'},
+	inst = { id = 2, cmd = 'npm i ', menu = 'Install'},
+	dev_inst = { id = 3, cmd = 'npm i --dev ', menu = 'Dev Install'},
+	global_inst = { id = 4, cmd = 'npm i -g ', menu = 'Global Install'},
+	uninst = { id = 5, cmd = 'npm un ', menu = 'Uninstall'},
+	globa_uninst = { id = 6, cmd = 'npm un -g ', menu = 'Global Uninstall'},
+	clean_inst = { id = 7, cmd = 'npm clean-install', menu = 'Clean Install'}
+}
+
 function OnExit(_, code)
 	print(vim.inspect(code))
 	vim.notify("Hello")
@@ -32,7 +42,7 @@ function build_input(cmd)
 		prompt = "> ",
 		default_value = '',
 		on_submit = function(val)
-			build_terminal('npm '..cmd:lower()..' '..val)
+			build_terminal(cmd.cmd..val)
 		end,
 	})
 	input:mount()
@@ -53,13 +63,16 @@ function build_popup()
 end
 
 function build_menu()
+	local values = vim.tbl_values(Npm)
+	local tab = {} 
+
+	for	index, value in ipairs(values) do
+		table.insert(tab, index, Menu.item(value.menu))
+	end
+
+	print(vim.inspect(tab))
 	return Menu(build_popup(), {
-		lines = {
-			Menu.item("Start"),
-			Menu.item("Install"),
-			Menu.item("Global Install"),
-			Menu.item("Clean Install")
-		},
+		lines = tab,
 		separator = {
 			char = '-',
 			text_align = 'left',
@@ -75,11 +88,11 @@ function build_menu()
 			print("Input closed")
 		end,
 		on_submit = function(value)
-			if value.text:find('Install') ~= nil then
-				-- input then npm.cmd
-				build_input(value.text)
+			print(vim.inspect(value))
+			if value.text:find('Install') ~= nil and value.text:find('Clean') == nil then
+				build_input(Npm[value._index])
 			else
-				build_terminal('npm '..value.text:lower())
+				build_terminal(Npm[value._index].cmd)
 			end
 		end
 	})
