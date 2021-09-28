0564078baba3aba11c2d48c5b7e15b6c5122fe9d
fedepujol <fpujol@protonmail.com>
2021-09-28 14:41:01 -0300

    Changes in tree behaviour

--- lua/pluggins/nvim-tree/init.lua
+++ lua/pluggins/nvim-tree/init.lua
@@ -8,6 +8,7 @@ vim.g.nvim_tree_hide_dotfiles = 0 				-- Hide dotfiles
 vim.g.nvim_tree_ignore = ignored 				-- Tell NvimTree to ingore this
 vim.g.nvim_tree_indent_markers = 1 				-- Show indent marks
 vim.g.nvim_tree_special_files = specialFiles 	-- List of elements that gets highlight with NvimTreeSpecialFile
+vim.g.nvim_tree_disable_window_picker = 1		-- Disable the window picker
 
 -- Redefined NvimTree icons for git information
 -- and Folders
@@ -40,12 +41,12 @@ require('nvim-tree').setup {
 	hijack_netrw = true, 		-- Hijack NetRW window on startup
 	open_on_setup = false, 		-- Dont open the tree when running this function
 	ignore_fg_on_setup = {}, 	-- Not open on setup if the filetype is in the list
-	auto_close = false, 		-- Dont close Neovim when the tree is the last window
+	auto_close = true, 			-- Close Neovim when the tree is the last window
 	open_on_tab = false, 		-- Open the tree when changing/opening a new tab
 	hijack_cursor = false,		-- Hijack the cursor to put it at the start of the filename
 	update_cwd = true, 			-- Updates the tree on :DirChanged
 	lsp_diagnostics = false, 	-- Dont show diagnostics in the sign-column
-	update_focused_file = {
+	update_focused_file = { 	-- Update the focused file on 'BufEnter', un-collapses the folder recursively until it finds the file
 		enable = true, 			-- Allow the cursor to be updated when entering another buffer
 		update_cwd = false,
 		ignore_list = {}
