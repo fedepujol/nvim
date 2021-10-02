0451cf6557ddd8c5af35a684ddb611bcd78f7643
fedepujol <fpujol@protonmail.com>
2021-10-02 10:01:32 -0300

    Update lsp structure

--- lua/pluggins/lsp/init.lua
+++ lua/pluggins/lsp/init.lua
@@ -1,18 +1,4 @@
--- Config
 require('pluggins.lsp.config')
-
--- LSP's
-require('pluggins.lsp.angular-ls')
-require('pluggins.lsp.bash-ls')
-require('pluggins.lsp.css-ls')
-require('pluggins.lsp.docker-ls')
-require('pluggins.lsp.efm-ls')
-require('pluggins.lsp.haskell-ls')
-require('pluggins.lsp.html-ls')
-require('pluggins.lsp.jdtls')
-require('pluggins.lsp.json-ls')
-require('pluggins.lsp.lua-ls')
-require('pluggins.lsp.pyright-ls')
-require('pluggins.lsp.tsserver-ls')
-require('pluggins.lsp.vim-ls')
-require('pluggins.lsp.yaml-ls')
+require('pluggins.lsp.saga')
+require('pluggins.lsp.installer')
+require('pluggins.lsp.servers')
---
+++ lua/pluggins/lsp/installer/init.lua
@@ -0,0 +1,10 @@
+-- Nvim-Lsp-Installer
+require('nvim-lsp-installer').settings{
+	ui = {
+		icons = {
+			server_installed = '﫟',
+			server_pending = '神',
+			server_uninstalled = ''
+		}
+	},
+}
