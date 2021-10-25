9381898f934aeceda3d3ed254f00c4a2e376a673
fedepujol <fpujol@protonmail.com>
2021-10-25 15:58:37 -0300

    Config borders for LSP's Hover and SignatureHelp (v0.6)

--- lua/plugins/lsp/config/handler.lua
+++ lua/plugins/lsp/config/handler.lua
@@ -1,14 +1,32 @@
+local border = {
+	{ "╭", "Normal"},
+	{ "─", "Normal"},
+	{ "╮", "Normal"},
+	{ "│", "Normal"},
+	{ "╯", "Normal"},
+	{ "─", "Normal"},
+	{ "╰", "Normal"},
+	{ "│", "Normal"}
+}
 -- Handlers
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 	vim.lsp.diagnostic.on_publish_diagnostics, {
 		underline = false,
-		virtual_text = true,
+		virtual_text = {
+			source = 'always',
+		},
 		signs = true,
 	}
 )
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
 	vim.lsp.handlers.hover, {
-		border = 'single'
+		border = border
+	}
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+	vim.lsp.handlers.signature_help, {
+		border = border
 	}
 )
