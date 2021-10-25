e0ba092bd346dfa8894b8d92f75176293468410d
fedepujol <fpujol@protonmail.com>
2021-10-25 15:55:21 -0300

    Implement better-escape

---
+++ lua/plugins/better-escape/init.lua
@@ -0,0 +1,9 @@
+-- BetterEscape
+
+require('better_escape').setup{
+	mapping = {'jk'}, 				-- Table mappings to use
+	timeout = vim.o.timeoutlen, 	-- Time in which the keys must be hit. Defaults to timeoutlen
+	clear_empty_lines = true, 		-- Clears the line if there is only whitespaces
+	keys = '<ESC>' 					-- Keys used for escaping. Could be a function
+}
+
