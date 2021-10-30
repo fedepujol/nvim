98fec208f792543be229f5f91a7c07eb48cff9c5
fedepujol <fpujol@protonmail.com>
2021-10-30 10:44:33 -0300

    Update config to latest changes in NvimTree

--- lua/plugins/nvim-tree/init.lua
+++ lua/plugins/nvim-tree/init.lua
@@ -1,14 +1,15 @@
 -- Nvim Tree Settings
-local specialFiles = {'README.md', 'Makefile', 'MAKEFILE'}
-local ignored = {'.git', 'node_modules'}
-
 vim.g.nvim_tree_git_hl = 1 						-- Enable git hightlights
 vim.g.nvim_tree_group_empty = 1 				-- Compact folders that only contain a single folder
-vim.g.nvim_tree_hide_dotfiles = 0 				-- Hide dotfiles
-vim.g.nvim_tree_ignore = ignored 				-- Tell NvimTree to ingore this
 vim.g.nvim_tree_indent_markers = 1 				-- Show indent marks
-vim.g.nvim_tree_special_files = specialFiles 	-- List of elements that gets highlight with NvimTreeSpecialFile
+vim.g.nvim_tree_special_files = { 				-- List of elements that gets highlight with NvimTreeSpecialFile
+	["Cargo.toml"] = true,
+	["Makefile"] = true,
+	["README.md"] = true,
+	["readme.md"] = true
+}
 vim.g.nvim_tree_disable_window_picker = 1		-- Disable the window picker
+vim.g.nvim_tree_create_in_closed_folder = 1		-- Create a file on a closed folder when the cursor is over it
 
 -- Redefined NvimTree icons for git information
 -- and Folders
@@ -48,19 +49,29 @@ require('nvim-tree').setup {
 	diagnostics = {
 		enable = false,
 	},
+	update_to_buf_dir = { 		-- Hijacks new directory buffers when they are opened
+		enable = true,
+		auto_open = true
+	},
 	update_focused_file = { 	-- Update the focused file on 'BufEnter', un-collapses the folder recursively until it finds the file
 		enable = true, 			-- Allow the cursor to be updated when entering another buffer
 		update_cwd = false,
-		ignore_list = {}
+		ignore_list = { 'node_modules', '.vscode' }
 	},
 	system_open = {
 		cmd = nil,
 		args = {}
 	},
 	view = {
-		width = 30,				-- Default width of the tree
-		side = 'left', 			-- Default side of the tree
-		auto_resize = true, 	-- Resize the tree when open a file
+		hide_root_folder = false, 	-- Hide path of current directory
+		width = 30,					-- Default width of the tree (only works with view.side = left|right)
+		height = 30,				-- Default height of the tree (only works with view.side = top|bottom)
+		side = 'left', 				-- Default side of the tree (could be left|right|bottom|top)
+		auto_resize = true, 		-- Resize the tree when open a file (it never works)
+	},
+	filters = {
+		custom = { '.git', 'node_modules', '.vscode', '.gitignore' }, 	-- Tell NvimTree to ingore this
+		dotfiles = false, 												-- Hide dotfiles
 	}
 }
 
