172f4c606965f48aedf15cbc1d8ffa9e1b65a9a8
fedepujol <fpujol@protonmail.com>
2021-11-02 18:53:05 -0300

    Update jdtls config

--- lua/plugins/lsp/servers/jdtls.lua
+++ lua/plugins/lsp/servers/jdtls.lua
@@ -1,8 +1,29 @@
 -- Java LSP
 
+local home = 'C:/Program Files/Java'
+
 require('lspconfig').jdtls.setup{
-	filetypes = {"java"},
-	init_options = {
-		jvm_args = {}
+	filetypes = { "java" },
+	settings = {
+		java = {
+			saveActions = {
+				organizeImports = true,
+			},
+			signatureHelp = {
+				enable = true,
+			},
+			configuration = {
+				runtimes = {
+					{
+						name = "JavaSE-1.8",
+						path = home..'/jdk1.8.0_144',
+					},
+					{
+						name = "JavaSE-11",
+						path = home..'/jdk-11.0.12',
+					}
+				}
+			}
+		}
 	}
 }
