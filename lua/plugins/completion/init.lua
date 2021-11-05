d30536253fa05b4b3c5580f148a83c543bbf2665
fedepujol <fpujol@protonmail.com>
2021-11-05 10:28:58 -0300

    Fix completion (add super-tab mapping)

--- lua/plugins/completion/init.lua
+++ lua/plugins/completion/init.lua
@@ -1,6 +1,15 @@
 -- Nvim-CMP
 local cmp = require('cmp')
 
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 cmp.setup({
 	snippet = {
 		expand = function(args)
@@ -12,8 +21,24 @@ cmp.setup({
 			behavior = cmp.ConfirmBehavior.Insert,
 			select = true
 		}),
-		["<S-TAB>"] = cmp.mapping.select_prev_item(),
-		["<TAB>"] = cmp.mapping.select_next_item(),
+		["<TAB>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif vim.fn["vsnip#available"](1) == 1 then
+				feedkey("<Plug>(vsnip-expand-or-jump)", "")
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, {'i', 's'}),
+		["<S-TAB>"] = cmp.mapping(function()
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+				feedkey("<Plug>(vsnip-jump-prev)", "")
+			end
+		end, {'i', 's'}),
 		["<C-SPACE>"] = cmp.mapping.complete(),
 		["<ESC>"] = cmp.mapping.close(),
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
--- lua/plugins/manager.lua
+++ lua/plugins/manager.lua
@@ -24,6 +24,7 @@ require('paq') {
 	'hrsh7th/cmp-nvim-lsp';
 	'hrsh7th/cmp-path';
 	'hrsh7th/cmp-vsnip';
+	'hrsh7th/vim-vsnip';
 
 	-- UI Components
 	'famiu/feline.nvim';
