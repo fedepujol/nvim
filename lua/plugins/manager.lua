46c0c7a47d5384f51fb457c44601fcf006ce4e98
fedepujol <fpujol@protonmail.com>
2021-11-12 17:39:02 -0300

    Add shipwright to export lush themes and bootstrap paq

--- lua/plugins/manager.lua
+++ lua/plugins/manager.lua
@@ -1,5 +1,11 @@
 -- Setting Paq
 
+local install_path = vim.fn.stdpath('data')..'/site/pack/paqs/start/paq-nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+	vim.fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
+end
+
 -- Paq Init
 require('paq') {
 	'savq/paq-nvim';
@@ -15,6 +21,7 @@ require('paq') {
 	-- Colors
 	'norcalli/nvim-colorizer.lua';
 	'rktjmp/lush.nvim';
+	'rktjmp/shipwright.nvim';
 	'fedepujol/nv-themes';
 
 	-- Completion
