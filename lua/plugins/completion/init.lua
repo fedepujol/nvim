2bf0ac411d5b7b86278df39391ca0a9fa6acec6f
fedepujol <9yjbd325@anonaddy.me>
2021-12-13 14:50:11 -0300

    Fix tabout issue with nvim-cmp

--- lua/plugins/completion/init.lua
+++ lua/plugins/completion/init.lua
@@ -1,11 +1,6 @@
 -- Nvim-CMP
 local cmp = require('cmp')
 
-local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 local feedkey = function(key, mode)
 	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
@@ -26,17 +21,17 @@ cmp.setup({
 				cmp.select_next_item()
 			elseif vim.fn["vsnip#available"](1) == 1 then
 				feedkey("<Plug>(vsnip-expand-or-jump)", "")
-			elseif has_words_before() then
-				cmp.complete()
 			else
 				fallback()
 			end
-		end, {'i', 's'}),
-		["<S-TAB>"] = cmp.mapping(function()
+		end,  {'i', 's'}),
+		["<S-TAB>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
 			elseif vim.fn["vsnip#jumpable"](-1) == 1 then
 				feedkey("<Plug>(vsnip-jump-prev)", "")
+			else
+			 	fallback()
 			end
 		end, {'i', 's'}),
 		["<C-SPACE>"] = cmp.mapping.complete(),
