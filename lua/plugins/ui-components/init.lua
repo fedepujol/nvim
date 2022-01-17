f261ea0ed906e2c5b56c3686bd064f9fa9370711
fedepujol <9yjbd325@anonaddy.me>
2022-01-17 12:26:22 -0300

    Remove feline

--- lua/plugins/ui-components/init.lua
+++ lua/plugins/ui-components/init.lua
@@ -1,5 +1,4 @@
 require('plugins.ui-components.file-explorer')
 require('plugins.ui-components.bufferline')
--- require('plugins.ui-components.statusline.feline')
 require('plugins.ui-components.statusline.heirline')
 require('plugins.ui-components.terminal')
