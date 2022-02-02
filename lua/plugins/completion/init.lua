f1a835d1647216ae6a4bc0aa4f69f55b6ea0b138
fedepujol <9yjbd325@anonaddy.me>
2022-02-02 09:34:13 -0300

    Fix nvim-cmp with tabout.nvim

--- lua/plugins/completion/init.lua
+++ lua/plugins/completion/init.lua
@@ -44,24 +44,24 @@ cmp.setup({
 			behavior = cmp.ConfirmBehavior.Insert,
 			select = true
 		}),
-		["<TAB>"] = cmp.mapping(function(fallback)
+		["<TAB>"] = function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif vim.fn["vsnip#available"](1) == 1 then
+			elseif vim.fn["vsnip#available"](1) ~=0 then
 				feedkey("<Plug>(vsnip-expand-or-jump)", "")
 			else
 				fallback()
 			end
-		end,  {'i', 's'}),
-		["<S-TAB>"] = cmp.mapping(function(fallback)
+		end,
+		["<S-TAB>"] = function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
-			elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+			elseif vim.fn["vsnip#jumpable"](-1) ~= 0 then
 				feedkey("<Plug>(vsnip-jump-prev)", "")
 			else
 				fallback()
 			end
-		end, {'i', 's'}),
+		end,
 		["<C-SPACE>"] = cmp.mapping.complete(),
 		["<ESC>"] = cmp.mapping.close(),
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
