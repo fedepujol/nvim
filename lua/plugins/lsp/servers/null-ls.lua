ead429db55ff36e596069748e1753bb54c343a94
fedepujol <9yjbd325@anonaddy.me>
2022-02-03 23:10:29 -0300

    Change in null-ls (tslint)

--- lua/plugins/lsp/servers/null-ls.lua
+++ lua/plugins/lsp/servers/null-ls.lua
@@ -1,16 +1,64 @@
 -- Null-ls
--- Performance goes to shit
 local null_ls = require('null-ls')
+local helpers = require('null-ls.helpers')
+local builtins = require('null-ls.builtins')
 
-local tslint = {
-	method = null_ls.methods.DIAGNOSTICS,
+local htmlBeautify = helpers.make_builtin({
+	name = 'htmlBeautify',
+	method = null_ls.methods.FORMATTING,
+	filetypes = { 'html' },
+	generator_opts = {
+		command = 'html-beautify',
+		args = helpers.range_formatting_args_factory({
+			'-t',
+			'-n',
+			'-w 110',
+			'-m 0',
+			"$FILENAME"
+		}),
+		to_stdin = true,
+	},
+	factory = helpers.formatter_factory
+})
+
+local pandoc = helpers.make_builtin({
+	name = 'pandoc',
+	method = null_ls.methods.FORMATTING,
+	filetypes = { 'markdown' },
+	generator_opts = {
+		command = 'pandoc',
+		args = helpers.range_formatting_args_factory({
+			'-f',
+			'markdown',
+			'-t',
+			'-gfm',
+			'-p',
+			"$FILENAME"
+		}),
+		on_output = function(params)
+			print(vim.inspect(params))
+		end,
+		to_stdin = true,
+	},
+	factory = helpers.formatter_factory
+})
+
+local tslint = helpers.make_builtin({
+	name = 'tslint',
+	method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
 	filetypes = { 'typescript' },
-	generator = null_ls.generator({
+	generator_opts = {
 		command = 'tslint',
+		args = {
+			'-t',
+			'json',
+			'-p',
+			vim.fn.getcwd(),
+			'$FILENAME'
+		},
 		to_stdin = true,
 		from_stderr = true,
 		format = 'json',
-		args = { '-t', 'json', '-p', vim.fn.getcwd() ,'$FILENAME' },
 		on_output = function(params)
 			local diagnostics = {}
 			for _, value in ipairs(params.output) do
@@ -26,11 +74,32 @@ local tslint = {
 			end
 			return diagnostics
 		end,
-	})
+	},
+	factory = helpers.generator_factory
+})
+
+local sources = {
+	-- Formatters
+	htmlBeautify,
+	pandoc,
+	builtins.formatting.prettier.with({
+		filetypes = { 'yaml' },
+		extra_args = {
+			'--use-tabs',
+			'-tab-width=4',
+		}
+	}),
+	-- Diagnostics
+	builtins.diagnostics.jsonlint,
+	builtins.diagnostics.markdownlint,
+	builtins.diagnostics.shellcheck,
+	tslint,
+	builtins.diagnostics.vint,
+	builtins.diagnostics.yamllint
 }
 
 null_ls.setup({
-	sources = tslint,
+	sources = sources,
 	debug = true,
 	on_attach = require('plugins.lsp.config').on_attach,
 	default_timeout = 50000,
