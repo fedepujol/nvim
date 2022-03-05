f677de4698343b2e8ef6f9ccc1e06b316f4b19c1
fedepujol <9yjbd325@anonaddy.me>
2022-03-05 10:27:21 -0300

    Disable rainbow (bug)

--- lua/plugins/treesitter/init.lua
+++ lua/plugins/treesitter/init.lua
@@ -1,14 +1,14 @@
 require('nvim-treesitter.configs').setup {
-    ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    indent = {
-        enable = true
-    },
-    highlight = {
-        enable = true,    -- false will disable the whole extension
+	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	indent = {
+		enable = true
+	},
+	highlight = {
+		enable = true,    -- false will disable the whole extension
 		disable = { 'html' },
-    },
+	},
 	rainbow = {
-		enable = true,
+		enable = false,
 		extended_mode = false,
 		max_file_lines = nil,
 	},
