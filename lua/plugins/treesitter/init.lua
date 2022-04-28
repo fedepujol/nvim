e8b75fc1678d77766db7a2c5bf188b1a0274b158
fedepujol <9yjbd325@anonaddy.me>
2022-04-28 13:17:02 -0300

    Change maintained for list of servers

--- lua/plugins/treesitter/init.lua
+++ lua/plugins/treesitter/init.lua
@@ -1,11 +1,30 @@
 require('nvim-treesitter.configs').setup({
-	ensure_installed = 'maintained', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	-- one of "all" or a list of languages
+	ensure_installed = {
+		'bash', 'c', 'c_sharp', 'cmake',
+		'cpp', 'css', 'dockerfile', 'go',
+		'graphql', 'haskell', 'help', 'hjson',
+		'html', 'http', 'java', 'javascript',
+		'jsdoc', 'json', 'json5', 'jsonc',
+		'llvm', 'lua', 'make', 'markdown',
+		'ninja', 'nix', 'pascal', 'perl',
+		'php', 'python', 'scheme', 'r',
+		'regex', 'rust', 'scss', 'teal',
+		'toml', 'tsx', 'typescript', 'verilog',
+		'vim', 'yaml',
+	},
+	sync_install = false, -- Install parsers synchronously (only applied to ensure_installed)
 	indent = {
 		enable = true,
 	},
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { 'html' },
+		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+		-- Using this option may slow down your editor, and you may see some duplicate highlights.
+		-- Instead of true it can also be a list of languages
+		additional_vim_regex_highlighting = false,
 	},
 	rainbow = {
 		enable = false,
