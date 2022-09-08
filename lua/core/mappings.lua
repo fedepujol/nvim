b745fb4001b2519784b98a73b6dad883011a47ba
fedepujol <9yjbd325@anonaddy.me>
2022-09-08 15:39:57 -0300

    upd: Update mappings to use vim.keymap.set

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,37 +1,37 @@
 -- General Mappings
-local opts = { noremap = true }
+local opts = { noremap = true, silent = true }
 
 -- Save
-vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', opts)
-vim.api.nvim_set_keymap('i', '<C-s>', '<ESC>:w<CR>', opts)
+vim.keymap.set('n', '<C-s>', ':w<CR>', opts)
+vim.keymap.set('i', '<C-s>', '<ESC>:w<CR>', opts)
 
 -- Quit
-vim.api.nvim_set_keymap('n', '<C-q>', ':q<CR>', opts)
+vim.keymap.set('n', '<C-q>', ':q<CR>', opts)
 
 -- Better Tabbing
-vim.api.nvim_set_keymap('v', '<S-TAB>', '<gv', opts)
-vim.api.nvim_set_keymap('v', '<TAB>', '>gv', opts)
+vim.keymap.set('v', '<S-TAB>', '<gv', opts)
+vim.keymap.set('v', '<TAB>', '>gv', opts)
 
 -- Window Nav
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', opts)
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', opts)
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', opts)
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', opts)
+vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
+vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
+vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
+vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
 
 -- Terminal
-vim.api.nvim_set_keymap('t', '<ESC>', '<C-\\><C-n>', { noremap = true })
-vim.api.nvim_set_keymap('t', '<A-h>', '<C-\\><C-N><C-w>h', { noremap = true })
-vim.api.nvim_set_keymap('t', '<A-j>', '<C-\\><C-N><C-w>j', { noremap = true })
-vim.api.nvim_set_keymap('t', '<A-k>', '<C-\\><C-N><C-w>k', { noremap = true })
-vim.api.nvim_set_keymap('t', '<A-l>', '<C-\\><C-N><C-w>l', { noremap = true })
+vim.keymap.set('t', '<ESC>', '<C-\\><C-n>', opts)
+vim.keymap.set('t', '<A-h>', '<C-\\><C-N><C-w>h', opts)
+vim.keymap.set('t', '<A-j>', '<C-\\><C-N><C-w>j', opts)
+vim.keymap.set('t', '<A-k>', '<C-\\><C-N><C-w>k', opts)
+vim.keymap.set('t', '<A-l>', '<C-\\><C-N><C-w>l', opts)
 
 -- Move Line/Block
-vim.api.nvim_set_keymap('n', '<A-j>', ':MoveLine(1)<CR>', { noremap = true, silent = true, nowait = true })
-vim.api.nvim_set_keymap('n', '<A-k>', ':MoveLine(-1)<CR>', { noremap = true, silent = true, nowait = true })
-vim.api.nvim_set_keymap('v', '<A-j>', ':MoveBlock(1)<CR>', { noremap = true, silent = true, nowait = true })
-vim.api.nvim_set_keymap('v', '<A-k>', ':MoveBlock(-1)<CR>', { noremap = true, silent = true, nowait = true })
+vim.keymap.set('n', '<A-j>', ':MoveLine(1)<CR>', opts)
+vim.keymap.set('n', '<A-k>', ':MoveLine(-1)<CR>', opts)
+vim.keymap.set('v', '<A-j>', ':MoveBlock(1)<CR>', opts)
+vim.keymap.set('v', '<A-k>', ':MoveBlock(-1)<CR>', opts)
 
-vim.api.nvim_set_keymap('n', '<A-h>', ':MoveHChar(-1)<CR>', { noremap = true, silent = true, nowait = true })
-vim.api.nvim_set_keymap('n', '<A-l>', ':MoveHChar(1)<CR>', { noremap = true, silent = true, nowait = true })
-vim.api.nvim_set_keymap('v', '<A-h>', ':MoveHBlock(-1)<CR>', { noremap = true, silent = true, nowait = true })
-vim.api.nvim_set_keymap('v', '<A-l>', ':MoveHBlock(1)<CR>', { noremap = true, silent = true, nowait = true })
+vim.keymap.set('n', '<A-h>', ':MoveHChar(-1)<CR>', opts)
+vim.keymap.set('n', '<A-l>', ':MoveHChar(1)<CR>', opts)
+vim.keymap.set('v', '<A-h>', ':MoveHBlock(-1)<CR>', opts)
+vim.keymap.set('v', '<A-l>', ':MoveHBlock(1)<CR>', opts)
