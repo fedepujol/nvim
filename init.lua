1febd649d86be7d1424b55505338f7b43f4b3418
fedepujol <9yjbd325@anonaddy.me>
2022-09-22 15:27:41 -0300

    upd: Config for neovide

--- init.lua
+++ init.lua
@@ -9,3 +9,11 @@ vim.cmd(':colo nv-cosmos')
 
 -- Init HeirLine so it takes the colors
 require('plugins.heirline')
+
+if vim.g.neovide ~= nil then
+	vim.opt.guifont = 'Fira Code:h11'
+	vim.g.neovide_confirm_quit = true
+	vim.g.neovide_rememeber_window_size = false
+	vim.g.neovide_cursor_animation_length = 0
+	vim.g.neovide_cursor_trail_size = 0
+end
