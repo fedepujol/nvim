cf4bc0928b486a0fd2358ad2d56b72d7689e5382
fedepujol <9yjbd325@anonaddy.me>
2022-09-22 15:27:00 -0300

    fix: Html highlight

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -19,7 +19,7 @@ require('nvim-treesitter.configs').setup({
 	},
 	highlight = {
 		enable = true, -- false will disable the whole extension
-		-- disable = { 'html' },
+		disable = { 'html' },
 		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
 		-- Using this option may slow down your editor, and you may see some duplicate highlights.
@@ -27,7 +27,7 @@ require('nvim-treesitter.configs').setup({
 		additional_vim_regex_highlighting = false,
 	},
 	rainbow = {
-		enable = false,
+		enable = true,
 		extended_mode = false,
 		max_file_lines = nil,
 	},
