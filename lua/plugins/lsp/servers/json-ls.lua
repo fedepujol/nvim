281093345b89b78c81258f9b4a7599d6ce8ff0d1
fedepujol <9yjbd325@anonaddy.me>
2023-01-02 10:46:41 -0300

    upd: json-ls

--- lua/plugins/lsp/servers/json-ls.lua
+++ lua/plugins/lsp/servers/json-ls.lua
@@ -5,14 +5,6 @@ local config = lsp.build_config()
 require('lspconfig').jsonls.setup({
 	on_attach = config.on_attach,
 	capabilities = lsp.capabilities,
-	root_dir = config.root_pattern({ '.git', vim.fn.getcwd() }),
-	commands = {
-		Format = {
-			function()
-				vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line('$'), 0 })
-			end,
-		},
-	},
 	settings = {
 		json = {
 			validate = {
