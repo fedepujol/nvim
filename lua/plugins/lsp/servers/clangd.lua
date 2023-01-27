b6882dcf58fee44260de133d9ccf7659a5b69289
fedepujol <9yjbd325@anonaddy.me>
2023-01-27 08:51:03 -0300

    del: plugin configs

--- lua/plugins/lsp/servers/clangd.lua
+++
@@ -1,8 +0,0 @@
--- Clangd Lsp
-local lsp = require('plugins.lsp.config')
-local config = lsp.build_config()
-
-require('lspconfig').clangd.setup({
-	on_attach = config.on_attach,
-	capabilities = config.capabilities,
-})
