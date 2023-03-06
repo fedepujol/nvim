93b217cff6fe7aaf0339070070de723a8dac1da5
fedepujol <9yjbd325@anonaddy.me>
2023-03-06 17:01:16 -0300

    upd: barbar

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -4,21 +4,29 @@ return {
 	'romgrk/barbar.nvim',
 	event = 'VeryLazy',
 	config = function()
-
-		vim.g.bufferline = {
+		require('bufferline').setup({
 			animation = false, -- Enable animations
 			auto_hide = false, -- Enable autohidding the bar when there is a single buffer
 			tabpages = false, -- Enable current/total tabpages indicator
 			closable = true, -- Enables close button
 			clickable = true, -- Enables clickable tabs. (Left-click goto buffer, middle-click closes)
+			diagnostics = {
+				{ enabled = false }, -- Error
+				{ enabled = false }, -- Warn
+				{ enabled = false }, -- Info
+				{ enabled = false }, -- Hint
+			},
+			exclude_name = { 'package.json' },
+			icons = true, -- Enable icons
+			icon_custom_colors = false,
 			icon_separator_active = '|', -- Separator icon for active tabs
 			icon_separator_inactive = '|', -- Separator icon for inactive tabs
-			icons = true, -- Enable icons
 			insert_at_end = false, -- Enables open new buffer at the end (Defaults to false -> open after current)
 			insert_at_start = false, -- Enables open new buffer at the start. Has priority over insert_at_end. (Defualts to false -> open after current)
-			maximum_length = 20, -- Maximum buffer name length
+			maximum_length = 50, -- Maximum buffer name length
+			semantic_letters = true,
 			no_name_title = 'Undefined', -- Sets the name of unnamed buffers. Only a static string is valid.
-		}
+		})
 
 		local opts = { noremap = true, silent = true }
 
