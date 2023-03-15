157f6dd3ca4ce76f1b1bfba1cf698be3e39ddb41
fedepujol <9yjbd325@anonaddy.me>
2023-03-15 09:34:56 -0300

    del: Neovide config

--- init.lua
+++ init.lua
@@ -1,10 +1,2 @@
 -- General bootstrap
 require('core')
-
-if vim.g.neovide ~= nil then
-	vim.opt.guifont = 'Fira Code:h11'
-	vim.g.neovide_confirm_quit = true
-	vim.g.neovide_rememeber_window_size = false
-	vim.g.neovide_cursor_animation_length = 0
-	vim.g.neovide_cursor_trail_size = 0
-end
