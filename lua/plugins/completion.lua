5e3fa2aad72379b1bce9e1a515ff66e84702e984
fedepujol <9yjbd325@anonaddy.me>
2023-04-19 10:23:51 -0300

    feat(completion): add parenthesis when selecting a method/function

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -127,5 +127,9 @@ return {
 				{ name = 'cmdline' },
 			}),
 		})
+
+		-- Add parenthesis to functions and methods
+		local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+		cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
 	end
 }
