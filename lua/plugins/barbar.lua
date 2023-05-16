8bfd58d145aefcb7f3b9e896be412f4c55a6982e
fedepujol <9yjbd325@anonaddy.me>
2023-05-16 18:32:50 -0300

    chore: update config of barbar

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -3,47 +3,47 @@
 return {
 	'romgrk/barbar.nvim',
 	event = 'VeryLazy',
-	config = function()
-		require('bufferline').setup({
-			animation = false, -- Enable animations
-			auto_hide = false, -- Enable autohidding the bar when there is a single buffer
-			tabpages = false, -- Enable current/total tabpages indicator
-			closable = true, -- Enables close button
-			clickable = true, -- Enables clickable tabs. (Left-click goto buffer, middle-click closes)
-			icons = {
-				buffer_index = false, -- Show buffer index
-				buffer_number = true, -- Show bufnr
-				diagnostics = {
-					{ enabled = false }, -- Error
-					{ enabled = false }, -- Warn
-					{ enabled = false }, -- Info
-					{ enabled = false }, -- Hint
-				},
-				filetype = {
-					custom_colors = false
-				},
+	keys = {
+		{ '<TAB>', ':BufferNext<CR>', desc = 'Next Buffer' },
+		{ '<S-TAB>', ':BufferPrevious<CR>', desc = 'Prev. Buffer'}
+	},
+	init = function()
+		vim.g.barbar_auto_setup = false
+	end,
+	opts = {
+		animation = false, -- Enable animations
+		icons = {
+			buffer_index = false, -- Show buffer index
+			buffer_number = true, -- Show bufnr
+			diagnostics = {
+				enabled = true
+			},
+			gitsigns = {
+				enabled = false
+			},
+			separator = {
+				left = '',
+				right = '',
+			},
+			inactive = {
 				separator = {
-					left = '|',
-					right = '|',
-				},
-				inactive = {
-					separator = {
-						left = '|',
-						right = '|'
-					}
+					left = '',
+					right = ''
 				}
 			},
-			insert_at_end = false, -- Enables open new buffer at the end (Defaults to false -> open after current)
-			insert_at_start = false, -- Enables open new buffer at the start. Has priority over insert_at_end. (Defualts to false -> open after current)
-			maximum_length = 50, -- Maximum buffer name length
-			semantic_letters = true,
-			no_name_title = 'Undefined', -- Sets the name of unnamed buffers. Only a static string is valid.
-		})
-
-		local opts = { noremap = true, silent = true }
-
-		-- Move between buffers
-		vim.keymap.set('n', '<TAB>', ':BufferNext<CR>', opts)
-		vim.keymap.set('n', '<S-TAB>', ':BufferPrevious<CR>', opts)
-	end
+			filename = {
+				custom_colors = true
+			}
+		},
+		sidebar_filetypes = {
+			['neo-tree'] = {
+				event = 'BufWipeout'
+			}
+		},
+		insert_at_end = false,  -- Enables open new buffer at the end (Defaults to false -> open after current)
+		insert_at_start = false, -- Enables open new buffer at the start. Has priority over insert_at_end. (Defualts to false -> open after current)
+		maximum_length = 50,    -- Maximum buffer name length
+		semantic_letters = true,
+		no_name_title = 'Undefined', -- Sets the name of unnamed buffers. Only a static string is valid.
+	}
 }
