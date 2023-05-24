310e0975b6b7cee4611406468ee0b727d89978a8
fedepujol <9yjbd325@anonaddy.me>
2023-05-24 18:10:41 -0300

    refactor: overhaul of toggleterm config

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,58 +3,13 @@
 return {
 	'akinsho/nvim-toggleterm.lua',
 	cmd = "ToggleTerm",
-	config = function()
-		local wk = require('which-key')
-		local Terminal = require('toggleterm.terminal').Terminal
-		local shell = vim.o.shell
-
-		require('toggleterm').setup({
-			size = 17,
-			hide_numbers = true,
-			start_in_insert = false,
-			direction = 'horizontal', -- vertical | float | tab
-			persist_size = true,
-			close_on_exit = true,
-			shell = shell
-		})
-
-		local lazygit = Terminal:new({
-			cmd = 'lazygit',
-			direction = 'tab',
-			on_open = function(term)
-				vim.cmd('startinsert!')
-				vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>Close<CR>', { noremap = true, silent = true })
-			end,
-			on_close = function(_)
-				-- Migrate to Neo-Tree
-			end,
-		})
-
-		function lazygit_toggle()
-			lazygit:toggle()
-		end
-
-		local diff = Terminal:new({
-			cmd = 'git diff',
-			direction = 'tab',
-			on_open = function(term)
-				vim.cmd('startinsert!')
-				vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', 'q', { noremap = true, silent = true })
-			end,
-			on_close = function(_)
-				print('Finished')
-			end,
-		})
-
-		function diff_toggle()
-			diff:toggle()
-		end
-
-		wk.register({
-			g = {
-				l = { '<cmd>lua lazygit_toggle()<CR>', 'Lazygit' },
-				d = { '<cmd>lua diff_toggle()<CR>', 'DiffView' },
-			},
-		}, { mode = 'n', prefix = '<leader>', noremap = true, silent = true })
-	end
+	opts = {
+		size = 17,
+		hide_numbers = true,
+		start_in_insert = false,
+		direction = 'horizontal', -- vertical | float | tab
+		persist_size = true,
+		close_on_exit = true,
+		open_mapping = '<leader>tc'
+	}
 }
