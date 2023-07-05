21badd25392997b9ae9a01be76c752f77bed6da0
fedepujol <9yjbd325@anonaddy.me>
2023-07-05 10:32:00 -0300

    chore(lazy): overhaul lazy config

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -1,82 +1,86 @@
 -- lazy.nvim Config
 
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable', -- latest stable release
 		lazypath,
 	})
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-		-- Spec
-		{
-			'fedepujol/cosmos.nvim',
-			lazy = false,
-			priority = 1000,
-			config = function()
-				vim.cmd([[color cosmos]])
-			end
-		},
-		{
-			'fedepujol/move.nvim',
-			cmd = {
-				"MoveLine", "MoveBlock", "MoveWord",
-			},
-		},
-		{
-			'rktjmp/lush.nvim',
-			cmd = 'Lushify',
-		},
-		{
-			'rktjmp/shipwright.nvim',
-			cmd = 'Shipwright',
-		},
-		{ 'folke/which-key.nvim' },
-		{
-			'mfussenegger/nvim-jdtls',
-			ft = 'java',
-			dependencies = {
-				'mfussenegger/nvim-dap'
-			}
-		},
-		{
-			'Bekaboo/dropbar.nvim',
-			lazy = false
-		},
-		{ import = 'plugins' },
-	},
-	-- Opts
+
+local spec = {
 	{
-		defaults = {
-			lazy = true
+		'fedepujol/cosmos.nvim',
+		lazy = false,
+		priority = 1000,
+		dev = true,
+		config = function()
+			vim.cmd([[color cosmos]])
+		end,
+	},
+	{
+		'fedepujol/move.nvim',
+		cmd = {
+			'MoveLine',
+			'MoveBlock',
+			'MoveWord',
 		},
-		dev = {
-			path = "~/sid/nvim"
+	},
+	{
+		'fedepujol/bracketpair.nvim',
+		dev = true,
+	},
+	{
+		'rktjmp/lush.nvim',
+		cmd = 'Lushify',
+	},
+	{
+		'rktjmp/shipwright.nvim',
+		cmd = 'Shipwright',
+	},
+	{
+		'mfussenegger/nvim-jdtls',
+		ft = 'java',
+		dependencies = {
+			'mfussenegger/nvim-dap',
 		},
-		ui = {
-			border = "rounded",
+	},
+	{ import = 'plugins' },
+}
+
+local opts = {
+	defaults = {
+		lazy = true,
+	},
+	dev = {
+		path = '~/sid/nvim',
+	},
+	ui = {
+		border = 'rounded',
+	},
+	diff = {
+		cmd = 'terminal.git',
+	},
+	checker = {
+		enabled = false,
+	},
+	change_detection = {
+		enabled = false,
+	},
+	performance = {
+		cache = {
+			enabled = true,
 		},
-		diff = {
-			cmd = 'terminal.git'
-		},
-		checker = {
-			enabled = false,
-		},
-		change_detection = {
-			enabled = false
-		},
-		performance = {
-			cache = {
-				enabled = true
-			}
-		},
-	})
+	},
+}
+
+require('lazy').setup(spec, opts)
