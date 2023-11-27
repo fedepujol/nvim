c951b1a5fe2e7b9d4de4f64c606922dfdead083c
fedepujol <9yjbd325@anonaddy.me>
2023-11-27 09:25:50 -0300

    feat(plugins): add fzfx

---
+++ lua/plugins/fzfx.lua
@@ -0,0 +1,30 @@
+-- Fzf Extended
+--
+return {
+	"linrongbin16/fzfx.nvim",
+	event = 'VimEnter',
+	keys = {
+		{
+			'<leader>fp',
+			"<cmd>FzfxFiles<CR>",
+			desc = 'Find Files',
+		},
+		{
+			'<leader>fg',
+			'<cmd>FzfxLiveGrep<CR>',
+			desc = 'Live Grep',
+		},
+	},
+	dependencies = {
+		{
+			"junegunn/fzf",
+			build = function()
+				vim.fn["fzf#install"]()
+			end,
+		},
+	},
+	config = function()
+		require("fzfx").setup()
+	end,
+
+}
