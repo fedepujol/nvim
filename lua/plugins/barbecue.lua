c8c5bbd7e68e9c2d9a5abef4eeb1a54489c31a06
fedepujol <9yjbd325@anonaddy.me>
2023-11-27 09:26:26 -0300

    feat(plugins): add barbecue

---
+++ lua/plugins/barbecue.lua
@@ -0,0 +1,25 @@
+return {
+	"utilyre/barbecue.nvim",
+	name = "barbecue",
+	event = "LspAttach",
+	dependencies = {
+		"SmiteshP/nvim-navic"
+	},
+	config = function()
+		require('barbecue').setup({
+			create_autocmd = false,
+		})
+
+		vim.api.nvim_create_autocmd({
+			"WinResized", -- or WinResized on NVIM-v0.9 and higher
+			"BufWinEnter",
+			"CursorHold",
+			"InsertLeave",
+		}, {
+			group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+			callback = function()
+				require("barbecue.ui").update()
+			end,
+		})
+	end
+}
