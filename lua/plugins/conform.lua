ccc73cb92ef8d03ec3a8df9c7d0094bee561d424
fedepujol <9yjbd325@anonaddy.me>
2024-04-09 09:50:25 -0300

    feat: Add yaml and xml formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,20 +1,22 @@
 return {
 	'stevearc/conform.nvim',
-	event = "LspAttach",
+	event = 'LspAttach',
 	config = function()
-		require("conform").setup({
+		require('conform').setup({
 			format_on_save = {
 				timeout_ms = 500,
-				lsp_fallback = true
+				lsp_fallback = true,
 			},
 			formatters_by_ft = {
-				css = { "prettierd" },
-				lua = { "stylua" },
-				markdown = { "prettierd" },
-				javascript = { "prettierd" },
-				rust = { "rustfmt" },
-				yaml = { "yamlfmt" }
+				css = { 'prettierd' },
+				lua = { 'stylua' },
+				markdown = { 'markdownlint' },
+				javascript = { 'prettierd' },
+				java = { 'google-java-format' },
+				rust = { 'rustfmt' },
+				yaml = { 'yamlfmt' },
+				xml = { 'xmlformatter' },
 			},
 		})
-	end
+	end,
 }
