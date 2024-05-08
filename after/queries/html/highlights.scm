38d403c3e799bf77bba93287b20bbb120c0ea6fc
fedepujol <9yjbd325@anonaddy.me>
2024-05-08 09:31:41 -0300

    chore(query): Remove query for angualr

--- after/queries/html/highlights.scm
+++
@@ -1,7 +0,0 @@
-((element (start_tag (tag_name)) (text) @property)
-	(#match? @property "\{"))
-
-(start_tag
-	(attribute (quoted_attribute_value (attribute_value) @method))
-	(#match? @method "[\(\)]")
-) 
