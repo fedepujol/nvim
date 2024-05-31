7b8a4904846d7e945cce22fdd8b486d74cf886db
fedepujol <9yjbd325@anonaddy.me>
2024-05-31 10:44:16 -0300

    style: Apply stylua

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -8,20 +8,20 @@ vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 
 -- Indentation
-vim.opt.autoindent = true -- Copy indent from current line when starting a new line
+vim.opt.autoindent = true  -- Copy indent from current line when starting a new line
 vim.opt.breakindent = true -- Wrapped lines will continue visually indented
-vim.opt.expandtab = false -- Converts tabs into spaces
-vim.opt.shiftwidth = 4 -- Change number of spaces inserted for indentation
+vim.opt.expandtab = false  -- Converts tabs into spaces
+vim.opt.shiftwidth = 4     -- Change number of spaces inserted for indentation
 vim.opt.smartindent = true -- Smart autoindenting when starting a new line
-vim.opt.smarttab = true -- Insert "tabstop" number of spaces when pressing tab
-vim.opt.softtabstop = -1 -- Length to use when editing text (0 for 'tabstop', -1 for 'shiftwidth')
-vim.opt.tabstop = 4 -- Set tab to 4 spaces
+vim.opt.smarttab = true    -- Insert "tabstop" number of spaces when pressing tab
+vim.opt.softtabstop = -1   -- Length to use when editing text (0 for 'tabstop', -1 for 'shiftwidth')
+vim.opt.tabstop = 4        -- Set tab to 4 spaces
 
 -- General UI
 vim.opt.background = 'dark' -- Tell NVim the background color
-vim.opt.cmdheight = 0 -- Number of screen lines to use for messages
-vim.opt.conceallevel = 0 -- Determine how text with the "conceal" attribute is shown. 0 -> As text
-vim.opt.cursorline = true -- Enable hightlightning of the current line
+vim.opt.cmdheight = 0       -- Number of screen lines to use for messages
+vim.opt.conceallevel = 0    -- Determine how text with the "conceal" attribute is shown. 0 -> As text
+vim.opt.cursorline = true   -- Enable hightlightning of the current line
 vim.opt.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 vim.opt.guifontwide = 'Fira Code:h11'
 vim.opt.laststatus = 3 -- Influence if the last window should have a statusline. 2 -> always.
@@ -41,7 +41,7 @@ vim.opt.termguicolors = true -- Set for colorizer. Enables 24Bit colors on TUI.
 vim.cmd(':lang en_GB')
 
 -- Encoding
-vim.opt.encoding = 'UTF-8' -- Encoding used internally
+vim.opt.encoding = 'UTF-8'     -- Encoding used internally
 vim.opt.fileencoding = 'UTF-8' -- Encoding fot the current buffer. Conversion done with iconv()
 
 -- Folds
@@ -51,25 +51,26 @@ vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
 
 -- Search
-vim.opt.hlsearch = true -- Highlighting search
-vim.opt.ignorecase = true -- Ignore case in search patterns
+vim.opt.hlsearch = true                             -- Highlighting search
+vim.opt.ignorecase = true                           -- Ignore case in search patterns
 vim.keymap.set('n', '<ESC>', '<CMD>nohlsearch<CR>') -- Remove search highlight
 
 -- General Behaviour
-vim.opt.backup = false -- No backup file when written
-vim.opt.clipboard = 'unnamedplus' -- Copy-Paste between Neovim and everything else
+vim.opt.backup = false                   -- No backup file when written
+vim.opt.clipboard = 'unnamedplus'        -- Copy-Paste between Neovim and everything else
 vim.opt.completeopt = 'menuone,noselect' -- Set for nvim-compe. Comma-separated options for insert mode.
-vim.opt.hidden = true -- Buffers becomes hidden when abandoned
-vim.opt.mouse = 'a' -- Enables mouse support
-vim.opt.shadafile = 'NONE' -- SHared DAta in between neovim sessions. Useful when you care
-vim.opt.splitbelow = true -- Horizontal splits will be bellow
-vim.opt.splitright = true -- Vertical splits will be on the right
-vim.opt.swapfile = false -- No SwapFile
-vim.opt.timeoutlen = 500 -- Time in miliseconds to wait for a mapped sequence to complete
+vim.opt.hidden = true                    -- Buffers becomes hidden when abandoned
+vim.opt.inccommand = 'split'
+vim.opt.mouse = 'a'                      -- Enables mouse support
+vim.opt.shadafile = 'NONE'               -- SHared DAta in between neovim sessions. Useful when you care
+vim.opt.splitbelow = true                -- Horizontal splits will be bellow
+vim.opt.splitright = true                -- Vertical splits will be on the right
+vim.opt.swapfile = false                 -- No SwapFile
+vim.opt.timeoutlen = 500                 -- Time in miliseconds to wait for a mapped sequence to complete
 vim.opt.undofile = true
 vim.opt.undolevels = 2000
-vim.opt.updatetime = 300 -- Time to wait before the swap file is written to disk (also CursorHold)
-vim.opt.wrap = true -- Display long lines as such
+vim.opt.updatetime = 300    -- Time to wait before the swap file is written to disk (also CursorHold)
+vim.opt.wrap = true         -- Display long lines as such
 vim.opt.writebackup = false -- No backup file
 vim.cmd('filetype plugin indent on')
 
