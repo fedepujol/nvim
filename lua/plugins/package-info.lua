c59e1cfacc49d98e5dd09b85627db85261ef8410
fedepujol <9yjbd325@anonaddy.me>
2024-07-18 09:11:09 -0300

    refactor(whichkey): New way of mapping

--- lua/plugins/package-info.lua
+++ lua/plugins/package-info.lua
@@ -4,39 +4,37 @@
 return {
 	'vuki656/package-info.nvim',
 	dependencies = {
-		'MunifTanjim/nui.nvim'
+		'MunifTanjim/nui.nvim',
 	},
 	ft = 'json',
 	config = function()
 		require('package-info').setup({
 			colors = {
-				up_to_date = "#3C4048", -- Text color for up to date dependency virtual text
-				outdated = "#d19a66", -- Text color for outdated dependency virtual text
+				up_to_date = '#3C4048', -- Text color for up to date dependency virtual text
+				outdated = '#d19a66', -- Text color for outdated dependency virtual text
 			},
 			icons = {
 				enable = true, -- Whether to display icons
 				style = {
-					up_to_date = "|  ", -- Icon for up to date dependencies
-					outdated = "|  ", -- Icon for outdated dependencies
+					up_to_date = '|  ', -- Icon for up to date dependencies
+					outdated = '|  ', -- Icon for outdated dependencies
 				},
 			},
-			autostart = false,     -- Whether to autostart when `package.json` is opened
+			autostart = false, -- Whether to autostart when `package.json` is opened
 			hide_up_to_date = true, -- It hides up to date versions when displaying virtual text
 			hide_unstable_versions = false, -- It hides unstable versions from version list e.g next-11.1.3-canary3
 			package_manager = 'npm',
 		})
 
 		local wk = require('which-key')
-		wk.register({
-			p = {
-				name = "PackageInfo",
-				c = { ':PackageInfoChangeVersion<CR>', "Change version"},
-				d = {':PackageInfoDelete<CR>', 'Delete dependency'},
-				h = {':PackageInfoHide<CR>', 'Hide versions'},
-				i = {':PackageInfoInstall<CR>', 'Install new dependency'},
-				s = {':PackageInfoShow<CR>', 'Show versions'},
-				u = {':PackageInfoUpdate<CR>', 'Update dependency'},
-			}
-		}, { mode = 'n', prefix = '<leader>', noremap = true, silent = true })
-	end
+		wk.add({
+			{ '<leader>p', group = 'PackageInfo' },
+			{ '<leader>pc', ':PackageInfoChangeVersion<CR>', desc = 'Change version' },
+			{ '<leader>pd', ':PackageInfoDelete<CR>', desc = 'Delete dependency' },
+			{ '<leader>ph', ':PackageInfoHide<CR>', desc = 'Hide versions' },
+			{ '<leader>pi', ':PackageInfoInstall<CR>', desc = 'Install new dependency' },
+			{ '<leader>ps', ':PackageInfoShow<CR>', desc = 'Show versions' },
+			{ '<leader>pu', ':PackageInfoUpdate<CR>', desc = 'Update dependency' },
+		})
+	end,
 }
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -4,6 +4,7 @@ return {
 	event = 'VeryLazy',
 	config = function()
 		require('which-key').setup({
+			preset = 'helix',
 			plugins = {
 				spelling = {
 					enable = false,
@@ -18,113 +19,76 @@ return {
 					g = true,
 				},
 			},
-			key_labels = {
-				['<space>'] = 'SPC',
-				['<CR>'] = 'RET',
-				['<Tab>'] = 'TAB',
-				['<leader>'] = 'LEAD',
-			},
 			icons = {
-				group = ' ',
-			},
-			window = {
-				border = 'single',
-				position = 'bottom',
+				group = '+',
 			},
 			show_help = true,
+			show_keys = true,
 		})
 
 		local wk = require('which-key')
 
-		wk.register({
-			s = {
-				name = 'Settings',
-				c = { ':CmpStatus<CR>', 'Completion Status' },
-				h = { ':checkhealth<CR>', 'Neovim Health' },
-				l = { ':Lazy<CR>', 'Lazy Info' },
-				m = { ':Mason<CR>', 'Mason Info' },
-				t = {
-					name = 'TreeSitter',
-					m = { ':TSModuleInfo<CR>', 'Module Info' },
-					i = { ':TSInstallInfo<CR>', 'Install Info' },
-				},
-			},
-			b = {
-				name = 'Buffers',
-				c = {
-					name = 'Close',
-					a = { ':BufferCloseAllButCurrent<CR>', 'All But Current' },
-					c = { ':BufferClose!<CR>', 'Current Buffer' },
-					l = { ':BufferCloseBuffersLeft<CR>', 'All Left Buffers' },
-					p = { ':BufferCloseAllButPinned<CR>', 'All But Pinned' },
-					r = { ':BufferCloseBuffersRight<CR>', 'All Right Buffers' },
-				},
-				m = {
-					name = 'Move Buffer',
-					f = { ':BufferMoveNext<CR>', 'Forwards' },
-					b = { ':BufferMovePrevious<CR>', 'Backwards' },
-				},
-				s = {
-					name = 'Sort',
-					b = { ':BufferOrderByBufferNumber<CR>', 'Buffer Number' },
-					d = { ':BufferOrderByDirectory<CR>', 'Directory' },
-					l = { ':BufferOrderByLanguage<CR>', 'Language' },
-					w = { ':BufferOrderByWindowNumber<CR>', 'Window Number' },
-				},
-				p = { ':BufferPick<CR>', 'Pick' },
-				o = { ':Neotree float buffers<CR>', 'Show Open Buffers' },
-			},
-			f = {
-				name = 'Files',
-				a = { ':normal!gg0vG$<CR>', 'Select all' },
-				d = { ':normal!$x<CR>', 'Delete last character' },
-			},
-			g = {
-				name = 'Git',
-				b = { "<cmd>lua require('gitsigns').blame_line()<CR>", 'Blame Line' },
-				h = {
-					name = 'Hunk',
-					j = { "<cmd>lua require('gitsigns').next_hunk()<CR>", 'Next Hunk' },
-					J = { "<cmd>lua require('gitsigns').prev_hunk()<CR>", 'Prev Hunk' },
-					p = { "<cmd>lua require('gitsigns').preview_hunk()<CR>", 'Preview Hunk' },
-					r = { "<cmd>lua require('gitsigns').reset_hunk()<CR>", 'Reset Hunk' },
-					R = { "<cmd>lua require('gitsigns').reset_buffer()<CR>", 'Reset Buffer' },
-				},
-			},
-			l = {
-				name = 'LSP',
-				i = { ':LspInfo<CR>', 'Info' },
-				b = {
-					name = 'Buffer',
-					i = { '<cmd>lua vim.lsp.buf.implementation()<CR>', 'Go to Implementation' },
-					s = { '<cmd>lua vim.lsp.buf.document_symbol()<CR>', 'Document Symbol' },
-					t = { '<cmd>lua vim.lsp.buf.type_definition()<CR>', 'Go to Type-Definition' },
-					f = { '<cmd>lua vim.lsp.buf.format({ async = true })<CR>', 'Format' },
-					h = { '<cmd>lua vim.lsp.buf.signature_help()<CR>', 'Get Signature Help' },
-					r = { ':Telescope lsp_references<CR>', 'Show References' },
-				},
-				d = {
-					name = 'Diagnostics',
-					n = { '<cmd>lua vim.diagnostic.goto_next()<CR>', 'Next Diagnostic' },
-					p = { '<cmd>lua vim.diagnostic.goto_prev()<CR>', 'Prev. Diagnostic' },
-				},
-				t = {
-					name = 'Trouble',
-					s = { ':Trouble symbols<cr>', 'Trouble Symbols' }
-				}
-			},
-			t = {
-				name = 'Terminal',
-				t = { ':ToggleTerm<CR>', 'Toggle Terminal' },
-				s = { ':TermSelect<CR>', 'Select Terminal' },
-			},
-		}, { mode = 'n', prefix = '<leader>', noremap = true, silent = true })
+		-- stylua: ignore
+		wk.add({
+			{ '<leader>s',   group = 'Settings' },
+			{ '<leader>sc',  ':CmpStatus<CR>',                                    desc = 'Completion Status' },
+			{ '<leader>sh',  ':checkhealth<CR>',                                  desc = 'Neovim Health' },
+			{ '<leader>sl',  ':Lazy<CR>',                                         desc = 'Lazy Info' },
+			{ '<leader>sm',  ':Mason<CR>',                                        desc = 'Mason Info' },
 
-		wk.register({
-			u = {
-				name = 'Utilities',
-				l = { ':normal!$x<CR>', 'Delete last character' },
-			},
-		}, { mode = 'v', prefix = '<leader>', noremap = true, silent = true })
+			{ '<leader>st',  group = "TreeSitter" },
+			{ '<leader>stm', ':TSModuleInfo<CR>',                                 desc = 'Module Info' },
+			{ '<leader>sti', ':TSInstallInfo<CR>',                                desc = 'Install Info' },
+
+			{ '<leader>b',   group = "Buffers" },
+			{ '<leader>bc',  group = "Close" },
+			{ '<leader>bca', ':BufferCloseAllButCurrent<CR>',                     desc = 'All But Current' },
+			{ '<leader>bcc', ':BufferClose!<CR>',                                 desc = 'Current Buffer' },
+			{ '<leader>bcp', ':BufferCloseAllButPinned<CR>',                      desc = 'All But Pinned' },
+			{ '<leader>bm',  group = "Move" },
+			{ '<leader>bmf', ':BufferMoveNext<CR>',                               desc = 'Forwards' },
+			{ '<leader>bmb', ':BufferMovePrevious<CR>',                           desc = 'Backwards' },
+			{ '<leader>bp',  ':BufferPick<CR>',                                   desc = 'Pick' },
+			{ '<leader>bo',  ':Neotree float buffers<CR>',                        desc = 'Show Open Buffers' },
+
+			{ '<leader>f',   group = "Files" },
+			{ '<leader>fa',  ':normal!gg0vG$<CR>',                                desc = 'Select all' },
+			{ '<leader>fb',  ':normal!$x<CR>',                                    desc = 'Delete last character' },
+
+			{ '<leader>i',   group = "Icons" },
+			{ '<leader>is',  '<CMD>Nerdy<CR>',                                    desc = 'Search icons' },
+
+			{ '<leader>g',   group = "Git" },
+			{ '<leader>gb',  "<cmd>lua require('gitsigns').blame_line()<CR>",     desc = 'Blame Line' },
+			{ '<leader>gh',  group = "Hunks" },
+			{ '<leader>ghj', "<cmd>lua require('gitsigns').next_hunk()<CR>",      desc = 'Next Hunk' },
+			{ '<leader>ghJ', "<cmd>lua require('gitsigns').prev_hunk()<CR>",      desc = 'Prev Hunk' },
+			{ '<leader>ghp', "<cmd>lua require('gitsigns').preview_hunk()<CR>",   desc = 'Preview Hunk' },
+			{ '<leader>ghr', "<cmd>lua require('gitsigns').reset_hunk()<CR>",     desc = 'Reset Hunk' },
+			{ '<leader>ghR', "<cmd>lua require('gitsigns').reset_buffer()<CR>",   desc = 'Reset Buffer' },
+
+			{ '<leader>l',   group = "Lsp" },
+			{ '<leader>li',  ':LspInfo<CR>',                                      desc = "Info" },
+			{ '<leader>lb',  group = "Buffer" },
+			{ '<leader>lbi', '<cmd>lua vim.lsp.buf.implementation()<CR>',         desc = 'Go to Implementation' },
+			{ '<leader>lbs', '<cmd>lua vim.lsp.buf.document_symbol()<CR>',        desc = 'Document Symbol' },
+			{ '<leader>lbt', '<cmd>lua vim.lsp.buf.type_definition()<CR>',        desc = 'Go to Type-Definition' },
+			{ '<leader>lbf', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>', desc = 'Format' },
+			{ '<leader>lbh', '<cmd>lua vim.lsp.buf.signature_help()<CR>',         desc = 'Get Signature Help' },
+			{ '<leader>lbr', ':Telescope lsp_references<CR>',                     desc = 'Show References' },
+			{ '<leader>ld',  group = "Diagnostics" },
+			{ '<leader>ldn', '<cmd>lua vim.diagnostic.goto_next()<CR>',           desc = 'Next Diagnostic' },
+			{ '<leader>ldp', '<cmd>lua vim.diagnostic.goto_prev()<CR>',           desc = 'Prev. Diagnostic' },
+			{ '<leader>lts', ':Trouble symbols<CR>',                              desc = 'Symbols' },
+
+			{ '<leader>t',   group = "Terminal" },
+			{ '<leader>tt',  ':ToggleTerm<CR>',                                   desc = 'Toggle Terminal' },
+			{ '<leader>ts',  ':TermSelect<CR>',                                   desc = 'Select Terminal' },
+		})
+
+		wk.add({
+			{ '<leader>u', group = 'Utilities' },
+			{ '<leader>ul', ':normal!$x<CR>', desc = 'Delete last character', mode = 'v' },
+		})
 	end,
 }
