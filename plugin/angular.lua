29bdfd5e92660ad45b14baf505646938fbf3855c
fedepujol <9yjbd325@anonaddy.me>
2024-08-05 14:36:26 -0300

    refactor(angular): New angular file name

--- plugin/angular.lua
+++ plugin/angular.lua
@@ -1,12 +1,12 @@
 vim.filetype.add({
 	pattern = {
-		['.*%.component%.html'] = 'angular.html', -- Sets the filetype to `angular.html` if it matches the pattern
+		['.*%.component%.html'] = 'htmlangular', -- Sets the filetype to `angular.html` if it matches the pattern
 	},
 })
 
 vim.api.nvim_create_autocmd('FileType', {
-	pattern = 'angular.html',
+	pattern = 'htmlangular',
 	callback = function()
-		vim.treesitter.language.register('angular', 'angular.html') -- Register the filetype with treesitter for the `angular` language/parser
+		vim.treesitter.language.register('angular', 'htmlangular') -- Register the filetype with treesitter for the `angular` language/parser
 	end,
 })
