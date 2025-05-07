6c3259cafa74d9f465dfb0d039a67384a8fe04aa
fedepujol <9yjbd325@anonaddy.me>
2025-05-07 09:37:41 -0300

    chore(gitsings): New mappings

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,7 +3,8 @@ return {
 	'lewis6991/gitsigns.nvim',
 	event = 'BufReadPre',
 	config = function()
-		require('gitsigns').setup({
+		local gitsigns = require('gitsigns')
+		gitsigns.setup({
 			signs = {
 				add          = { text = '│' },
 				change       = { text = '│' },
@@ -24,5 +25,12 @@ return {
 				border = 'rounded',
 			},
 		})
+
+		vim.keymap.set('n', '<leader>gb', gitsigns.toggle_current_line_blame, { desc = "[g]it [b]lame" })
+		vim.keymap.set('n', '<leader>ghn', function() gitsigns.nav_hunk('next') end, { desc = "[g]it [h]unk [n]ext" })
+		vim.keymap.set('n', '<leader>ghp', function() gitsigns.nav_hunk('prev') end, { desc = "[g]it [h]unk [p]rev" })
+		vim.keymap.set('n', '<leader>ghP', gitsigns.preview_hunk, { desc = "[g]it [h]unk [P]review" })
+		vim.keymap.set('n', '<leader>grh', gitsigns.reset_hunk, { desc = "[g]it [r]eset [h]unk" })
+		vim.keymap.set('n', '<leader>grb', gitsigns.reset_buffer, { desc = "[g]it [r]eset [b]uffer" })
 	end,
 }
