3ba21c1c4d058033e22ca6862c96cab3abb75e46
fedepujol <9yjbd325@anonaddy.me>
2025-08-20 18:17:56 -0300

    chore: remove mapping from toggleterm and add local options for norg files

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -19,13 +19,12 @@ return {
 		toggleterm.setup({
 			start_in_insert = false,
 			direction = 'tab', -- vertical | float | tab
-			open_mapping = '<leader>to',
+			open_mapping = nil,
 			insert_mapping = false,
 			shell = newShell,
 		})
 
 		vim.keymap.set('n', '<leader>tt', toggleterm.toggle, { desc = '[t]erminal [t]oggle' })
 		vim.keymap.set('n', '<leader>ts', ':TermSelect<CR>', { desc = '[t]erminal [s]elect' })
-		-- vim.keymap.set('n', '<leader>to', toggleterm.new, { desc = '[t]erminal [o]pen' })
 	end,
 }
